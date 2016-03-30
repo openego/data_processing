d85c3a1dd74e78e5f44d250ebc313b381f49e50a
Ludwig Schneider <ludwig.schneider@rl-institut.de>
2016-03-30 21:04:17 +0200

    loads updated

--- ego_deu_loads/ego_deu_loads.sql
+++ ego_deu_loads/ego_deu_loads.sql
@@ -40,7 +40,7 @@ CREATE TABLE		orig_geo_ego.ego_deu_loads_collect_buffer100 (
 		geom geometry(Polygon,3035),
 CONSTRAINT 	ego_deu_loads_collect_buffer100_pkey PRIMARY KEY (id));
 
--- "Insert Buffer"   (OK!) 2.811.000ms =151.437
+-- "Insert Buffer"   (OK!) 2.800.000ms =151.436
 INSERT INTO	orig_geo_ego.ego_deu_loads_collect_buffer100 (geom)
 	SELECT	(ST_DUMP(ST_MULTI(ST_UNION(
 			ST_BUFFER(ST_TRANSFORM(poly.geom,3035), 100)
@@ -62,52 +62,52 @@ ALTER TABLE		orig_geo_ego.ego_deu_loads_collect_buffer100 OWNER TO oeuser;
 ---------- ---------- ---------- ---------- ---------- ----------
 
 -- "Create Table"   (OK!) 100ms =0
-DROP TABLE IF EXISTS	orig_geo_ego.ego_deu_loads_buffer100_unbuffer;
-CREATE TABLE	orig_geo_ego.ego_deu_loads_buffer100_unbuffer (
+DROP TABLE IF EXISTS	orig_geo_ego.ego_deu_loads_collect_buffer100_unbuffer;
+CREATE TABLE		orig_geo_ego.ego_deu_loads_collect_buffer100_unbuffer (
 		id SERIAL,
 		geom geometry(Polygon,3035),
-CONSTRAINT	ego_deu_loads_buffer100_unbuffer_pkey PRIMARY KEY (id));
+CONSTRAINT	ego_deu_loads_collect_buffer100_unbuffer_pkey PRIMARY KEY (id));
 
 -- "Insert Buffer"   (OK!) 350.000ms =202.130
-INSERT INTO	orig_geo_ego.ego_deu_loads_buffer100_unbuffer (geom)
+INSERT INTO	orig_geo_ego.ego_deu_loads_collect_buffer100_unbuffer (geom)
 	SELECT	(ST_DUMP(ST_MULTI(ST_UNION(
 			ST_BUFFER(ST_TRANSFORM(buffer.geom,3035), -100)
 		)))).geom ::geometry(Polygon,3035) AS geom
-	FROM	orig_geo_ego.ego_deu_loads_buffer100 AS buffer
+	FROM	orig_geo_ego.ego_deu_loads_collect_buffer100 AS buffer
 	GROUP BY buffer.id
 	ORDER BY buffer.id;
 
--- "Create Index GIST (geom)"   (OK!) 2.000ms =0
-CREATE INDEX	ego_deu_loads_buffer100_unbuffer_geom_idx
-	ON	orig_geo_ego.ego_deu_loads_buffer100_unbuffer
+-- "Create Index GIST (geom)"   (OK!) 4.000ms =0
+CREATE INDEX	ego_deu_loads_collect_buffer100_unbuffer_geom_idx
+	ON	orig_geo_ego.ego_deu_loads_collect_buffer100_unbuffer
 	USING	GIST (geom);
 
 -- "Grant oeuser"   (OK!) -> 100ms =0
-GRANT ALL ON TABLE	orig_geo_ego.ego_deu_loads_buffer100_unbuffer TO oeuser WITH GRANT OPTION;
-ALTER TABLE		orig_geo_ego.ego_deu_loads_buffer100_unbuffer OWNER TO oeuser;
+GRANT ALL ON TABLE	orig_geo_ego.ego_deu_loads_collect_buffer100_unbuffer TO oeuser WITH GRANT OPTION;
+ALTER TABLE		orig_geo_ego.ego_deu_loads_collect_buffer100_unbuffer OWNER TO oeuser;
 
 ---------- ---------- ---------- ---------- ---------- ----------
 -- "Cutting"
 ---------- ---------- ---------- ---------- ---------- ----------
 
 -- "Create Table"   (OK!) 100ms =
-DROP TABLE IF EXISTS	orig_geo_ego.ego_deu_loads_buffer100_unbuffer_cut;
-CREATE TABLE	orig_geo_ego.ego_deu_loads_buffer100_unbuffer_cut (
+DROP TABLE IF EXISTS	orig_geo_ego.ego_deu_loads_collect_buffer100_unbuffer_cut;
+CREATE TABLE	orig_geo_ego.ego_deu_loads_collect_buffer100_unbuffer_cut (
 		id SERIAL,
 		geom geometry(Polygon,3035),
-CONSTRAINT	ego_deu_loads_buffer100_unbuffer_cut_pkey PRIMARY KEY (id));
+CONSTRAINT	ego_deu_loads_collect_buffer100_unbuffer_cut_pkey PRIMARY KEY (id));
 
 -- "Insert Cut"   (OK!) 350.000ms =
-INSERT INTO	orig_geo_ego.ego_deu_loads_buffer100_unbuffer_cut (geom)
+INSERT INTO	orig_geo_ego.ego_deu_loads_collect_buffer100_unbuffer_cut (geom)
 	SELECT	ST_INTERSECTION(poly.geom,cut.geom) ::geometry(Polygon,3035) AS geom
-	FROM	orig_geo_ego.ego_deu_loads_buffer100_unbuffer AS poly,
+	FROM	orig_geo_ego.ego_deu_loads_collect_buffer100_unbuffer AS poly,
 		calc_gridcells_znes.znes_deu_gridcells_qgis AS cut
-	GROUP BY poly.id
-	ORDER BY poly.id;
+	GROUP BY poly.id, cut.geom
+	ORDER BY poly.id, cut.geom;
 
 -- "Create Index GIST (geom)"   (OK!) 2.000ms =0
-CREATE INDEX	ego_deu_loads_buffer100_unbuffer_cut_geom_idx
-	ON	orig_geo_ego.ego_deu_loads_buffer100_unbuffer_cut
+CREATE INDEX	ego_deu_loads_collect_buffer100_unbuffer_cut_geom_idx
+	ON	orig_geo_ego.ego_deu_loads_collect_buffer100_unbuffer_cut
 	USING	GIST (geom);
 
 -- "Grant oeuser"   (OK!) -> 100ms =0
