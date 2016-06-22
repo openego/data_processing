8fedb3aad3e623a84e833a93e23610b6f144ea0c
apoorva patrikar <apoorvapatrikar.27@gmail.com>
2016-06-22 14:06:08 -0700

    included nodes and edges as components of a graph

--- graphml-parsing/GraphClass.py
+++ graphml-parsing/GraphClass.py
@@ -1,13 +1,18 @@
+import pprint
 class Graph:
    'Common base class for all nodes'
    graphCount = 0
 
-   def __init__(self, name, position):
+   def __init__(self, name, nodes, edges):
       self.name = name
-      self.position = position
+      self.nodes = nodes
+      self.edges = edges
       Graph.graphCount += 1
       Graph.displayName(self)
 
    def displayName(self):
-      print("Graph ",self.graphCount,". Label= ", self.name)
+      print("\nGraph ",self.graphCount,". Label= ", self.name," has nodes ")
+      for x in self.nodes:
+         print(x.name, end=', ')
+
 
