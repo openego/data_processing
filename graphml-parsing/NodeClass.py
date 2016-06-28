93d2adad2c6e41fac3b295505429e777d9fec95b
apoorva patrikar <apoorvapatrikar.27@gmail.com>
2016-06-28 13:16:17 -0700

    Specified Change in the edge class
    i forgot to upload this version earlier!

--- graphml-parsing/NodeClass.py
+++ graphml-parsing/NodeClass.py
@@ -5,9 +5,9 @@ class Node(Graph):
    nodeCount = 0
 
    def __init__(self, name, position):
-      #self.name = name
-      #self.position = position
-      super().__init__(name, position)
+      self.name = name
+      self.position = position
+      #super().__init__(name, position)
       Node.nodeCount += 1
       #Node.displayName(self)
 
