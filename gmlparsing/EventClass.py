029244c2f78ef80a93b873a3957787fa7ad1b1a3
apoorva patrikar <apoorvapatrikar.27@gmail.com>
2016-07-21 12:41:49 -0700

    updated with parent node of events

--- gmlparsing/EventClass.py
+++ gmlparsing/EventClass.py
@@ -4,13 +4,16 @@ class Event(Node):
    'Common base class for all Events'
    eventCount = 0
 
-   def __init__(self, name, type):
+   def __init__(self, name, type, innode):
       self.name = name
       self.type = type
-      #super().__init__(name)
+      self.position = innode
+      #super().__init__(innode.name,1)
       Event.eventCount += 1
       #Event.displayName(self)
 
    def displayName(self):
-      print("Event Label : ", self.name," of type ",self.type)
-
+      if "EVENT_CHARACTERISTIC_START" == self.type:
+         print("\n");
+      print("Event Label : ", self.name," of type ",self.type, " under node ",self.position.name)
+      #print("Event Label : ", self.name, " of type ", self.type, " under node ", super().name)
