4c4cc5d126b66c2180475a48446807a267cffc89
apoorva patrikar <apoorvapatrikar.27@gmail.com>
2016-07-22 05:42:45 -0700

    stack data structure

---
+++ gmlparsing/Stack.py
@@ -0,0 +1,12 @@
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def is_empty(self):
+        return (self.items == [])
