5798ee37bfa2eccef2668bbc01021f1278c1cb92
apoorva patrikar <apoorvapatrikar.27@gmail.com>
2016-07-25 12:46:53 -0700

    ordered objects appropriately

--- gmlparsing/MakeObjects.py
+++ gmlparsing/MakeObjects.py
@@ -6,6 +6,7 @@ from gmlparsing.NodeClass import Node
 from gmlparsing.LaneClass import Lane
 from gmlparsing.TaskClass import Task
 from gmlparsing.DataObjClass import DataObj
+from gmlparsing.AnnotsClass import Annots
 from gmlparsing.EventClass import Event
 from gmlparsing.EdgeClass import Edge
 from gmlparsing.PoolClass import Pool
@@ -14,6 +15,7 @@ from gmlparsing.GatewayClass import Gateway
 
 
 tree = ET.parse('ego_deu_loads_per_grid_district.graphml')
+#tree = ET.parse('test.graphml')
 root = tree.getroot()
 
 ns = {'gml': 'http://graphml.graphdrawing.org/xmlns',
@@ -23,8 +25,11 @@ allStartEv = []
 alltasks= []
 alledges = []
 alldos = []
+allanns = []
 allGateways = []
 allObj = []
+allpnodes = []
+allPool = []
 
 def makeGraph(graphs):
     nodeArray = []
@@ -34,12 +39,15 @@ def makeGraph(graphs):
 
     for gIterator in graphs:
         nodes = gIterator.findall('gml:node',ns)
+
         size = len(nodes)
         if (size > 0):
-            if size == 1:
-                nodeArray=makeNode(nodes)
-            else:
-                nodeArray.extend(makeNode(nodes))
+         #   if size == 1:
+         #       nodeArray=makeNode(nodes)
+         #   else:
+         nodeArray.extend(makeNode(nodes))
+
+
 
         edges = gIterator.findall('gml:edge', ns)
         size = len(edges)
@@ -54,8 +62,23 @@ def makeGraph(graphs):
 
 def makeNode(nodes):
     tmpArray = []
+    lvl5=None
     for nIterator in nodes:
-        tmpArray.append(Node(nIterator.attrib["id"], 1))
+        nodename = nIterator.attrib["id"]
+
+        if nIterator.find('tn:NodeLabel', ns):
+            nodename = nIterator.find('tn:NodeLabel', ns).text
+        tmpArray.append(Node(nodename, nIterator.attrib["id"]))
+
+
+        if 'yfiles.foldertype' in nIterator.attrib:
+            for groups in nIterator.iter('{http://www.yworks.com/xml/graphml}GenericGroupNode'):
+                labels = groups.findall('tn:NodeLabel', ns)
+                for l in labels:
+                    if l.text and tmpArray[-1] not in allpnodes and ':' in nIterator.attrib["id"]:
+                        tmpArray[-1].name = l.text.replace('\n', ' ').replace('\r', '')
+                        allpnodes.append(tmpArray[-1])
+
 
         graphs = nIterator.findall('gml:graph', ns)
         if (len(graphs) > 0):
@@ -66,14 +89,14 @@ def makeNode(nodes):
             activities = d.findall('tn:GenericNode',ns)
             if(activities):
                 allStartEv.extend(makeEvent(activities,tmpArray[-1]))
-                alldos.extend(makeDataObj(activities,tmpArray[-1]))
-                alltasks.extend(makeTask(activities,tmpArray[-1].name))
+                alldos.extend(makeDataObj(activities,tmpArray[-1],1))
+                allanns.extend(makeDataObj(activities, tmpArray[-1], 2))
+                alltasks.extend(makeTask(activities,tmpArray[-1]))
                 allGateways.extend(makeGateway(activities,tmpArray[-1]))
 
-            pools = d.findall('tn:TableNode',ns)
-            if(pools):
-                makePool(pools)
-
+        pools = d.findall('tn:TableNode',ns)
+        if(pools):
+            allPool.extend(makePool(pools,tmpArray[-1].name))
     return tmpArray
 
 def makeEdge(edges):
@@ -107,15 +130,26 @@ def makeTask(activities,pos):
             tmpArray.append(Task(name, pos))
     return tmpArray
 
-def makeDataObj(activities,pos):
-    tmpArray = []
+def makeDataObj(activities,pos,num):
+    tmpArray1 = []
+    tmpArray2 = []
     for aIterator in activities:
         if "com.yworks.bpmn.Artifact.withShadow" in aIterator.attrib.values():
             obj = aIterator.find('tn:NodeLabel', ns);
-            if(obj and len(obj.text.strip())>0):
+            if obj:
                 name = obj.text
-                tmpArray.append(DataObj(name, pos))
-    return tmpArray
+            obj2 = aIterator.find('tn:StyleProperties', ns).findall('tn:Property', ns);
+            for anns in obj2:
+                if "ARTIFACT_TYPE_ANNOTATION" in anns.attrib.values():
+                    #print(obj.text,' is annotation')
+                    tmpArray2.append(Annots(name, pos))
+                if obj and "ARTIFACT_TYPE_DATA_OBJECT" in anns.attrib.values():
+                    #print(obj.text,' is do')
+                    tmpArray1.append(DataObj(name, pos))
+    if num==1:
+        return tmpArray1
+    else:
+        return tmpArray2
 
 def makeEvent(activities, innode):
     tmpArray = []
@@ -142,16 +176,17 @@ def makeGateway(activities,pos):
     return tmpArray
 
 
-def makePool(pools):
+def makePool(pools,pos):
     tmpArray = []
     laneArray = []
     for pIterator in pools:
         if "com.yworks.bpmn.Pool" in pIterator.attrib.values():
             completePool = pIterator.findall('tn:NodeLabel', ns)
             name = completePool[0].text
-            tmpArray.append(Pool(name, 1))
+            tmpArray.append(Pool(name, pos))
             completePool.remove(completePool[0])
             laneArray.append(makeLane(completePool,name))
+
     return tmpArray
 
 def makeLane(lanes,parent):
@@ -171,86 +206,214 @@ starts=[]
 mids=[]
 ends=[]
 s = Stack()
+sForSub = Stack()
+sublist = []
+sublist2 = []
+sgFor = None
+sgIndex = 0
+sg = False
 
 def getStart():         # fixed start and end at events
     currInd=-1
+    sgFor = None
+    sgIndex = 0
+    sgIndex2 = 0
+    sg = False
+    sg2 = False
+    newSub = '-'
 
     for ase in allStartEv:
+
         if type(ase) is Event and ase.type == 'EVENT_CHARACTERISTIC_START':
+
             starts.append(ase.position.name)
             parsing.append(ase.type)
-            parsing_nodes.append(ase.position)
+            parsing_nodes.append(ase.position.name)
+
+            if sgFor and sgFor in ase.position.name:
+                sublist.insert(0,parsing[-1])
+                parsing[-1]=''
+                sg=True
+            else:
+                sg = False
+                sublist = []
             currInd += 1
+
+            if newSub is not None and newSub != '-' and newSub in ase.position.name:
+                sgIndex2 = parsing.index(sublist2)
+                sublist2.insert(0, parsing[-1])
+                del parsing[-1]
+                sg2 = True
 
             for ae in alledges:
                 if ae.source == ase.position.name:   #if current item has outgoing edge
+                    for ap in allpnodes:
+                        if ae.target == ap.position:
+                            sgFor = ap.position
+
+                            currInd = checkOtherStarts(ae.target, currInd, sg,sublist,sg2,sublist2,s)
+                            while not s.is_empty():
+                                parsing.append(s.pop())
+                            parsing.append(ap.name)
+                            parsing.append(getAnnot(ap))
+
+                            sgIndex = parsing.index(ap.name)+1
+                            parsing_nodes.append(ap.position)
+
+                            sub = checkNext(ae.target, currInd, sg,sublist,sg2,sublist2,s)
+                            if sub is not None:
+                                newSub = sub
+                            break
+
                     for t in alltasks:
-                        if ae.target == t.position:
-                            currInd = checkOtherStarts(ae.target, 1,currInd)
-                            parsing.append(t.name)
-                            parsing_nodes.append(t.position)
+                        if ae.target == t.position.name:
+                            currInd = checkOtherStarts(ae.target,currInd, sg,sublist,sg2,sublist2,s)
 
                             while not s.is_empty():
                                 parsing.append(s.pop())
+                                assign(sg,sg2,sublist,sublist2,newSub,ase,'start')
 
+                            parsing.append(t.name)
+                            assign(sg, sg2, sublist, sublist2, newSub, ase,'start')
+
+                            parsing.append(getAnnot(t))
+                            assign(sg, sg2, sublist, sublist2, newSub, ase,'start')
+
+                            parsing_nodes.append(t.position.name)
                             allStartEv.insert(currInd + 1, t)  # dynamic list tht has all parsing elements
-                            #print(t.name)
-
                             currInd += 1
-                            checkNext(ae.target,currInd)
+
+                            sub = checkNext(ae.target,currInd,sg,sublist,sg2,sublist2,s)
+                            if sub is not None:
+                                newSub = sub
+
+                    if sg==True:
+                        parsing.insert(sgIndex, sublist)
+
+
 
         if type(ase) is Event and (ase.type == 'EVENT_CHARACTERISTIC_INTERMEDIATE_CATCHING' or ase.type == 'EVENT_CHARACTERISTIC_END'):
-            #starts.append(ase.position.name)
             parsing.append(ase.type)
             currInd += 1
 
-def checkNext(next,currInd):
+
+def assign(sg,sg2,sublist,sublist2,newSub,ase,callfrom):
+    if callfrom == 'start':
+        if sg == True and parsing[-1] is not None:
+            sublist.append(parsing[-1])
+            del parsing[-1]
+        if newSub is not None and newSub != '-' and newSub in ase.position.name and parsing[-1] is not None:
+            sublist2.append(parsing[-1])
+            del parsing[-1]
+
+    elif callfrom == 'chnext':
+        if sg == True and parsing[-1] is not None:
+            sublist.append(parsing[-1])
+            del parsing[-1]
+        if sg2 == True and parsing[-1] is not None:
+            sublist2.append(parsing[-1])
+            del parsing[-1]
+
+def getAnnot(t):
+    answer = None
+    checkWith = None
+    if type(t) is Task:
+        checkWith = t.position.name
+    else:
+        checkWith = t.position
+    for ae in alledges:
+        if ae.target == checkWith or ae.source == checkWith:
+            for aa in allanns:
+                if aa.name is not None and ae.source == aa.position.name:
+                    answer = aa.name
+                    break
+    return answer
+
+
+def checkNext(next,currInd,sg,sublist,sg2,sublist2,s):
+    sub = None
     for ae in alledges:
         if ae.source == next:
             newObj = findType(ae.target)
 
             if type(newObj) is DataObj and newObj.position.name not in parsing_nodes:
-                checkOtherStarts(ae.target, 3, currInd)
+                checkOtherStarts(ae.target, currInd, sg,sublist,sg2,sublist2,s)
                 while not s.is_empty():
                     parsing.append(s.pop())
-
+                    assign(sg,sg2,sublist,sublist2,None,None,'chnext')
                 parsing.append(newObj.name)
+                assign(sg, sg2, sublist, sublist2, None, None, 'chnext')
+
                 parsing_nodes.append(newObj.position.name)
-                checkNext(newObj.position.name,currInd)
+                checkNext(newObj.position.name,currInd,sg,sublist,sg2,sublist2,s)
 
-            if type(newObj) is Task and newObj.position not in parsing_nodes:
-                checkOtherStarts(ae.target, 3, currInd)
+            if type(newObj) is Task and newObj.position.name not in parsing_nodes:
+                checkOtherStarts(ae.target, currInd, sg,sublist,sg2,sublist,s)
                 while not s.is_empty():
                     parsing.append(s.pop())
+                    assign(sg, sg2, sublist, sublist2, None, None, 'chnext')
 
                 parsing.append(newObj.name)
-                parsing_nodes.append(newObj.position)
-                checkNext(newObj.position,currInd)
+                assign(sg, sg2, sublist, sublist2, None, None, 'chnext')
 
-def checkOtherStarts(target,ttype,ind):
+                parsing_nodes.append(newObj.position.name)
+                parsing.append(getAnnot(newObj))
+                assign(sg, sg2, sublist, sublist2, None, None, 'chnext')
+
+                checkNext(newObj.position.name, currInd,sg,sublist,sg2,sublist2,s)
+
+            if type(newObj) is Node and newObj.position not in parsing_nodes:
+                checkOtherStarts(ae.target, currInd, sg,sublist,sg2,sublist2,s)
+                while not s.is_empty():
+                    parsing.append(s.pop())
+                parsing.append(newObj.name)
+
+                sgIndex = parsing.index(newObj.name)+1
+
+                #sg = True
+                sub = newObj.position
+                parsing_nodes.append(newObj.position)
+
+                parsing.append(getAnnot(newObj))
+                checkNext(newObj.position, currInd, sg,sublist,sg2,sublist2,s)
+
+                parsing.insert(sgIndex, sublist2)
+                #allStartEv.insert(currInd , newObj)
+
+
+
+    return sub
+
+def checkOtherStarts(target,ind, sg,sublist,sg2,sublist2,s):
     for ae in alledges:
         if(ae.target==target and ae.source not in starts):
             newElement = findType(ae.source)
             newType = type(newElement)
-            #print('another one for ',newElement.name)
 
             if newElement != '':
                 if newType is DataObj and newElement.position.name not in parsing_nodes:
-                    ind += 1
-                    #print('appended',newElement.name)
                     s.push(newElement.name)
-                    #parsing.append(newElement.name)
+                    parsing_nodes.append(newElement.position.name)
+                    ind = checkOtherStarts(newElement.position.name, ind, sg, sublist, sg2, sublist2, s)
+
+                if newType is Task and newElement.position.name not in parsing_nodes:
+                    ind = checkOtherStarts(newElement.position.name, ind, sg, sublist, sg2, sublist2,s)
+                    while not s.is_empty():
+                        parsing.append(s.pop())
+                        assign(sg, sg2, sublist, sublist2, None, None, 'chnext')
+                    parsing.append(newElement.name)
+                    assign(sg, sg2, sublist, sublist2, None, None, 'chnext')
                     parsing_nodes.append(newElement.position.name)
 
-                    ind = checkOtherStarts(newElement.position.name,2,ind) #going to the beginning in reverse
+                if newType is Node and newElement.position not in parsing_nodes:
+                    ind = checkOtherStarts(newElement.position, ind, sg, sublist, sg2, sublist2,s)
+                    while not s.is_empty():
+                        parsing.append(s.pop())
+                        assign(sg, sg2, sublist, sublist2, None, None, 'chnext')
+                    parsing.append(newElement.name)
+                    assign(sg, sg2, sublist, sublist2, None, None, 'chnext')
+                    parsing_nodes.append(newElement.position)
 
-                else:
-                    if newType is Task and newElement.position not in parsing_nodes:
-                        s.push(newElement.name)
-                        #parsing.append(newElement.name)
-                        parsing_nodes.append(newElement.position)
-                        ind = checkOtherStarts(newElement.position, 2, ind)
-                        #print(newElement.position)
     return ind
 
 def findType(input): # fixed start and end at events
@@ -262,17 +425,35 @@ def findType(input): # fixed start and end at events
 
     if  answer=='' :
         for t in alltasks:
-            if input == t.position:
+            if input == t.position.name:
                 answer = t
                 break
+
+    if answer == '':
+        for ap in allpnodes:
+            if input == ap.position:
+                answer = ap
+                break
+
+    if answer == '':
+        for e in allStartEv:
+            if type(e) is Event and input == e.position.name:
+                answer = e
+                break
+
+    if answer == '':
+        for g in allGateways:
+            if type(g) is Gateway and input == g.position.name:
+                answer = g
+                break
+
     return answer
 
 getStart()
 
+
 for x in parsing:
     if x=='EVENT_CHARACTERISTIC_START':
         print('')
+    #print(parsing.index(x),': ',x)
     print(x)
-
-
-
