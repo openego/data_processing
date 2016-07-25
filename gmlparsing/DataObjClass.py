d9e6f672445d235798342fcc99880413c1eb82d2
apoorva patrikar <apoorvapatrikar.27@gmail.com>
2016-07-25 12:57:26 -0700

    added location information display

--- gmlparsing/DataObjClass.py
+++ gmlparsing/DataObjClass.py
@@ -1,14 +1,15 @@
 from gmlparsing.NodeClass import Node
 
 class DataObj(Node):
-   'Common base class for all Data Objectss'
+   'Common base class for all Data Objects'
    dataObjCount = 0
 
    def __init__(self, name, position):
       super().__init__(name, position)
+      self.name = self.name.replace('\n', ' ').replace('\r', '')
       DataObj.dataObjCount += 1
       #DataObj.displayName(self)
 
    def displayName(self):
-      print("Data Object Label : ", self.name)
+      print("Data Object Label : ", self.name,' at ',self.position.name)
 
