819cfce8509f2b86593ff5e3e59c63b9fd1dc517
gplessm <guido.plessmann@rl-institut.de>
2016-09-14 08:40:48 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='data_processing',
+    version='0.1-pre',
+    packages=[''],
+    url='https://github.com/openego/data_processing',
+    license='GNU GENERAL PUBLIC LICENSE Version 3',
+    author='open_eGo development group',
+    author_email='',
+    description='Data processing related to the research project open_eGo'
+)
