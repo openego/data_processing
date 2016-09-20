6c94a1e4f11ace504e539f5fb299fbaccf574195
apoorva patrikar <apoorvapatrikar.27@gmail.com>
2016-09-20 06:49:46 -0700

    change of name

--- gmlparsing/AnnotsClass.py
+++ gmlparsing/AnnotsClass.py
@@ -1,13 +1,13 @@
 from gmlparsing.NodeClass import Node
 
-class Annotation(Node):
+class Annots(Node):
    'Common base class for all Annotations'
    dataObjCount = 0
 
    def __init__(self, name, position):
       super().__init__(name, position)
       self.name = self.name.replace('\n', ' ').replace('\r', '')
-      Annotation.dataObjCount += 1
+      Annots.dataObjCount += 1
       #Annots.displayName(self)
 
    def displayName(self):
