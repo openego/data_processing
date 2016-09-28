bc273f298cdd6a6ad27ec06724f055a266785ada
prathamesh-pradhan <prathamesh.pradhan9@gmail.com>
2016-09-28 23:59:46 +0200

    run any graphml file

--- gmlparsing/RunTool.py
+++ gmlparsing/RunTool.py
@@ -1,8 +1,22 @@
 from sys import argv;
-from gmlparsing import MakeObjects;
+import MakeObjects;
+from tkinter import filedialog,messagebox;
 
-def run(MakeObjects):
+print(__name__)
+def run():
+    filename = filedialog.askopenfilename()
+    myFile = filename.split('/')
     print("Executed Successfully!")
+    fileNameLength = len(myFile)
+    if fileNameLength <=1:
+        print("Empty file")
+        
+        if messagebox.askyesno('Try Again',"You didn't selected the file, Retry?"):
+            run()
+    else:
+        myFileName = myFile[-1]
+        MakeObjects.execGraphmlFile(myFileName)
+    #return myFileName
     
 if __name__ == '__main__':
-        run(argv[0])
+        run()
