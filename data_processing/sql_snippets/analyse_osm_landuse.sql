d43c1a52de62d2efcfe81a03d3bdb507f7515d15
Ludwig Huelk <ludwig.huelk@rl-institut.de>
2016-10-13 00:18:20 +0200

    osm landuse analyse

--- data_processing/sql_snippets/analyse_osm_landuse.sql
+++ data_processing/sql_snippets/analyse_osm_landuse.sql
@@ -2,6 +2,36 @@
 OSM test for landuse
 */ 
 
+-- 2016-10-01 openstreetmap
+DROP MATERIALIZED VIEW IF EXISTS	openstreetmap.osm_deu_landuse_mview CASCADE;
+CREATE MATERIALIZED VIEW 		openstreetmap.osm_deu_landuse_mview AS
+	SELECT	osm.gid, osm.tags, osm.geom
+	FROM	openstreetmap.osm_deu_polygon AS osm
+	WHERE	tags ? 'landuse'
+	ORDER BY osm.osm_id;
+
+-- "Create Index GIST (geom)"   (OK!) -> 6.000ms =0
+CREATE INDEX  	osm_deu_landuse_mview_geom_idx
+	ON	openstreetmap.osm_deu_landuse_mview
+	USING	GIST (geom);
+
+-- "Grant oeuser"   (OK!) -> 100ms =0
+GRANT ALL ON TABLE	openstreetmap.osm_deu_landuse_mview TO oeuser WITH GRANT OPTION;
+ALTER TABLE		openstreetmap.osm_deu_landuse_mview OWNER TO oeuser;
+
+-- Scenario eGo data processing
+INSERT INTO	scenario.eGo_data_processing_clean_run (version,schema_name,table_name,script_name,entries,status,user_name,timestamp)
+	SELECT	'0.1' AS version,
+		'openstreetmap' AS schema_name,
+		'osm_deu_landuse_mview' AS table_name,
+		'analyse_osm_landuse.sql' AS script_name,
+		COUNT(geom)AS entries,
+		'OK' AS status,
+		session_user AS user_name,
+		NOW() AT TIME ZONE 'Europe/Berlin' AS timestamp
+	FROM	openstreetmap.osm_deu_landuse_mview;
+
+/* 
 -- 2016-01-13 orig_osm
 DROP MATERIALIZED VIEW IF EXISTS	openstreetmap.osm_deu_landuse_20160113_mview CASCADE;
 CREATE MATERIALIZED VIEW 		openstreetmap.osm_deu_landuse_20160113_mview AS
@@ -91,9 +121,8 @@ INSERT INTO	scenario.eGo_data_processing_clean_run (version,schema_name,table_na
 		'OK' AS status,
 		NOW() AT TIME ZONE 'Europe/Berlin' AS timestamp
 	FROM	openstreetmap.osm_deu_landuse_20161005_mview;
+*/ 
 
-
---SELECT session_user AS user_name;
 
 /*
 	SELECT	COUNT(osm.*) AS cnt
