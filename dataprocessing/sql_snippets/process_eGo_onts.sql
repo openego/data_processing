5e82c32de45c88ce4eeaead6d33bdb42b21e65a4
Jonas Guetter <jonas.guetter@rl-institut.de>
2016-10-27 15:35:41 +0200

    changed table names

--- dataprocessing/sql_snippets/process_eGo_onts.sql
+++ dataprocessing/sql_snippets/process_eGo_onts.sql
@@ -17,16 +17,16 @@ SELECT ('POLYGON((0 0, 0 '||$4||', '||$3||' '||$4||', '||$3||' 0,0 0))')::geomet
 $$ LANGUAGE sql IMMUTABLE STRICT;
 
 
-CREATE TABLE IF NOT EXISTS calc_ego_grid_district."ego_deu_ontgrids"
+CREATE TABLE IF NOT EXISTS model_draft."ego_grid_lv_ontgrids"
 (
   id serial NOT NULL,
   geom geometry(Polygon,3035),
   load_area_id integer,
-  CONSTRAINT test_ego_deu_ontgrids_pkey PRIMARY KEY (id)
+  CONSTRAINT ego_grid_lv_ontgrids_pkey PRIMARY KEY (id)
 );
 
-TRUNCATE TABLE  calc_ego_grid_district."ego_deu_ontgrids";
-INSERT INTO calc_ego_grid_district."ego_deu_ontgrids" (geom, load_area_id)
+TRUNCATE TABLE  model_draft."ego_grid_lv_ontgrids";
+INSERT INTO model_draft."ego_grid_lv_ontgrids" (geom, load_area_id)
 
 -- Erstelle ein Gitternetz auf der bbox der Lastgebiete:
 
@@ -59,31 +59,31 @@ INSERT INTO calc_ego_grid_district."ego_deu_ontgrids" (geom, load_area_id)
 
 	END AS geom, area.id AS load_area_id
 
-FROM calc_ego_loads."ego_deu_load_area" AS area INNER JOIN calc_ego_loads."calc_ego_peak_load" AS peak_load ON peak_load.id = area.id;
+FROM model_draft."ego_demand_loadarea" AS area INNER JOIN model_draft."ego_demand_load_area_peak_load" AS peak_load ON peak_load.id = area.id;
 
 
 
-CREATE TABLE IF NOT EXISTS calc_ego_substation."ego_deu_onts"
+CREATE TABLE IF NOT EXISTS model_draft."ego_grid_mvlv_substation"
 (
   geom geometry(Point,3035),
   id serial NOT NULL,
   is_dummy boolean,
   subst_id integer,
   load_area_id integer,
-  CONSTRAINT ego_deu_onts_pkey PRIMARY KEY (id)
+  CONSTRAINT ego_grid_mvlv_substation_pkey PRIMARY KEY (id)
 );
   
-TRUNCATE TABLE calc_ego_substation."ego_deu_onts";
-INSERT INTO calc_ego_substation."ego_deu_onts" (geom, load_area_id)
+TRUNCATE TABLE model_draft."ego_grid_mvlv_substation";
+INSERT INTO model_draft."ego_grid_mvlv_substation" (geom, load_area_id)
 
 -- Bestimme diejenigen Mittelpunkte der Grid-Polygone, die innerhalb der Lastgebiete liegen:
 SELECT DISTINCT ST_CENTROID (grids.geom)::geometry(POINT,3035) AS geom, area.id AS load_area_id
-FROM calc_ego_grid_district."ego_deu_ontgrids" AS grids, calc_ego_loads."ego_deu_load_area" AS area
+FROM model_draft."ego_grid_lv_ontgrids" AS grids, model_draft."ego_demand_loadarea" AS area
 WHERE ST_WITHIN(ST_CENTROID (grids.geom),area.geom)
 	AND area.id = grids.load_area_id;
 
 -- Füge den Lastgebieten, die aufgrund ihrer geringen Fläche keine ONTs zugeordnet bekommen haben, ihren Mittelpunkt als ONT-STandort hinzu: (01:54 min)
-INSERT INTO calc_ego_substation."ego_deu_onts" (geom, load_area_id)
+INSERT INTO model_draft."ego_grid_mvlv_substation" (geom, load_area_id)
 
 SELECT
 CASE WHEN ST_CONTAINS (geom,ST_CENTROID(area_without_ont.geom)) THEN 
@@ -99,10 +99,10 @@ END, area_without_ont.id
 FROM
 
 	(SELECT geom, id
-	FROM calc_ego_loads."ego_deu_load_area"
+	FROM model_draft."ego_demand_loadarea"
 	EXCEPT
 	SELECT l_area.geom AS geom, l_area.id
-	FROM calc_ego_loads."ego_deu_load_area" AS l_area, calc_ego_substation."ego_deu_onts" AS ont
+	FROM model_draft."ego_demand_loadarea" AS l_area, model_draft."ego_grid_mvlv_substation" AS ont
 	WHERE ST_CONTAINS (l_area.geom, ont.geom)
 	GROUP BY (l_area.id))
 	AS area_without_ont
@@ -110,28 +110,28 @@ FROM
 
 	
 -- Lege Buffer um ONT-STandorte und ermittle die Teile der Lastgebiete, die sich nicht innerhalb dieser Buffer befinden
-CREATE TABLE IF NOT EXISTS calc_ego_grid_district."ego_deu_load_area_rest"
+CREATE TABLE IF NOT EXISTS model_draft."ego_grid_lv_loadarearest"
 (
   id serial NOT NULL,
   geom geometry(Polygon,3035),
   load_area_id integer NOT NULL,
-  CONSTRAINT test_ego_deu_load_area_rest_pkey PRIMARY KEY (id)
+  CONSTRAINT ego_grid_lv_loadarearest_pkey PRIMARY KEY (id)
 );
 
-TRUNCATE TABLE calc_ego_grid_district."ego_deu_load_area_rest";
-INSERT INTO calc_ego_grid_district."ego_deu_load_area_rest" (geom, load_area_id)
+TRUNCATE TABLE model_draft."ego_grid_lv_loadarearest";
+INSERT INTO model_draft."ego_grid_lv_loadarearest" (geom, load_area_id)
 
 SELECT (ST_DUMP(ST_DIFFERENCE(load_area.geom, area_with_onts.geom))).geom::geometry(Polygon,3035) AS geom, load_area.id AS load_area_id
 
 FROM
 	(SELECT ST_BUFFER(ST_UNION(onts.geom),576) AS geom,l_area.id AS id
-	FROM calc_ego_substation."ego_deu_onts" AS onts, calc_ego_loads."ego_deu_load_area" AS l_area
+	FROM model_draft.ego_grid_mvlv_substation AS onts, model_draft.ego_demand_loadarea AS l_area
 	WHERE ST_CONTAINS (l_area.geom,onts.geom) AND (l_area.geom && onts.geom)
 	GROUP BY l_area.id) AS area_with_onts
 
 	INNER JOIN
 
-	calc_ego_loads."ego_deu_load_area" AS load_area
+	model_draft.ego_demand_loadarea AS load_area
 
 	ON (load_area.id = area_with_onts.id )
 
@@ -140,24 +140,24 @@ WHERE  ST_AREA(ST_DIFFERENCE(load_area.geom, area_with_onts.geom)) > 0;
 
   
 -- Bestimme die Mittelpunkte der Gebiete, die noch nicht durch ONT abgedeckt sind, und lege diese Mittelpunkte als ONT-Standorte fest
-INSERT INTO calc_ego_substation."ego_deu_onts" (geom, load_area_id)
-SELECT ST_CENTROID(geom)::geometry(POINT,3035), load_area_id FROM calc_ego_grid_district."ego_deu_load_area_rest"
+INSERT INTO model_draft.ego_grid_mvlv_substation (geom, load_area_id)
+SELECT ST_CENTROID(geom)::geometry(POINT,3035), load_area_id FROM model_draft."ego_grid_lv_loadarearest"
 ;
 
 
 -- Setze dummy-flag auf false für alle erzeugten ONTs
-UPDATE calc_ego_substation."ego_deu_onts" 
+UPDATE model_draft."ego_grid_mvlv_substation" 
 SET is_dummy = FALSE;
 
 -- Füge Information für die substation_id hinzu
 
-UPDATE 	calc_ego_substation.ego_deu_onts AS t1
+UPDATE 	model_draft."ego_grid_mvlv_substation" AS t1
 SET  	subst_id = t2.subst_id
 FROM    (
 	SELECT	ont.id AS id,
 		gd.subst_id AS subst_id
-	FROM	calc_ego_substation.ego_deu_onts AS ont,
-		calc_ego_grid_district.grid_district AS gd
+	FROM	model_draft."ego_grid_mvlv_substation" AS ont,
+		calc_ego_grid_district."model_draft.ego_grid_mv_griddistrict" AS gd
 	WHERE  	gd.geom && ont.geom AND
 		ST_CONTAINS(gd.geom,ont.geom)
 	) AS t2
@@ -165,13 +165,13 @@ WHERE  	t1.id = t2.id;
 
 -- Lösche Hilfstabellen 
 
-DROP TABLE IF EXISTS calc_ego_grid_district."ego_deu_ontgrids";
-DROP TABLE IF EXISTS calc_ego_grid_district."ego_deu_load_area_rest";
+DROP TABLE IF EXISTS model_draft."ego_grid_lv_ontgrids";
+DROP TABLE IF EXISTS model_draft."ego_grid_lv_loadarearest";
 
 -- Set comment on table
-COMMENT ON TABLE calc_ego_substation.ego_deu_onts IS
+COMMENT ON TABLE model_draft."ego_grid_mvlv_substation" IS
 '{
-"Name": "eGo data processing - ego_deu_onts",
+"Name": "eGo data processing - ego_grid_mvlv_substation",
 "Source": [{
                   "Name": "open_eGo",
                   "URL":  "https://github.com/openego/data_processing" }],
@@ -206,7 +206,11 @@ COMMENT ON TABLE calc_ego_substation.ego_deu_onts IS
                 {"Name": "Jonas Gütter",
                 "Mail": "jonas.guetter@rl-institut.de",
                 "Date":  "20.10.2016",
-                "Comment": "Created table" }],
+                "Comment": "Created table" },
+				{"Name": "Jonas Gütter",
+                "Mail": "jonas.guetter@rl-institut.de",
+                "Date":  "27.10.2016",
+                "Comment": "Changed table names" }],
 
 "ToDo": [""],
 "Licence": ["tba"],
@@ -214,13 +218,13 @@ COMMENT ON TABLE calc_ego_substation.ego_deu_onts IS
 }'; 
 
 -- Select description
-SELECT obj_description('calc_ego_substation.ego_deu_onts'::regclass)::json;
+SELECT obj_description('model_draft.ego_grid_mvlv_substation'::regclass)::json;
 
 -- Add entry to scenario logtable
 INSERT INTO	scenario.eGo_data_processing_clean_run (version,schema_name,table_name,script_name,entries,status,user_name,timestamp)
 SELECT	'0.1' AS version,
 	'model_draft' AS schema_name,
-	'ego_deu_onts' AS table_name,
+	'ego_grid_mvlv_substation' AS table_name,
 	'process_eGo_onts.sql' AS script_name,
 	COUNT(*)AS entries,
 	'OK' AS status,
