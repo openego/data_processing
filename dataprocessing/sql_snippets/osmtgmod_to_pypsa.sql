6c927cf469c81a7d06586512ccb1449127783abe
Ilka <ilka.cussmann@fh-flensburg.de>
2016-10-27 11:55:29 +0200

    adjust sql snippet osmtgmod_to_pypsa to new db structure

--- dataprocessing/sql_snippets/osmtgmod_to_pypsa.sql
+++ dataprocessing/sql_snippets/osmtgmod_to_pypsa.sql
@@ -1,17 +1,17 @@
 -- osmTGmod2pyPSA
 
 -- CLEAN UP OF TABLES
-TRUNCATE calc_ego_hv_powerflow.bus CASCADE;
-TRUNCATE calc_ego_hv_powerflow.line CASCADE;
-TRUNCATE calc_ego_hv_powerflow.transformer CASCADE;
+TRUNCATE model_draft.ego_grid_pf_hv_bus CASCADE;
+TRUNCATE model_draft.ego_grid_pf_hv_line CASCADE;
+TRUNCATE model_draft.ego_grid_pf_hv_transformer CASCADE;
 
 -- BUS DATA
-INSERT INTO calc_ego_hv_powerflow.bus (bus_id, v_nom, geom)
+INSERT INTO model_draft.ego_grid_pf_hv_bus (bus_id, v_nom, geom)
 SELECT 
   bus_i AS bus_id,
   base_kv AS v_nom,
   geom
-  FROM calc_ego_osmtgmod.bus_data
+  FROM grid.otg_ehvhv_bus_data
   WHERE result_id = 1;
 
 -- Scenario eGo data processing
@@ -23,10 +23,10 @@ INSERT INTO	scenario.eGo_data_processing_clean_run (version,schema_name,table_na
 		COUNT(bus_id)AS entries,
 		'OK' AS status,
 		NOW() AT TIME ZONE 'Europe/Berlin' AS timestamp
-FROM	calc_ego_hv_powerflow.bus;
+FROM	model_draft.ego_grid_pf_hv_bus;
 
 -- BRANCH DATA
-INSERT INTO calc_ego_hv_powerflow.line (line_id, bus0, bus1, x, r, b, s_nom, cables, frequency, geom, topo)
+INSERT INTO model_draft.ego_grid_pf_hv_line (line_id, bus0, bus1, x, r, b, s_nom, cables, frequency, geom, topo)
 SELECT 
   branch_id AS line_id,
   f_bus AS bus0,
@@ -39,7 +39,7 @@ SELECT
   frequency,
   geom,
   topo
-  FROM calc_ego_osmtgmod.branch_data
+  FROM grid.otg_ehvhv_branch_data
   WHERE result_id = 1 and (link_type = 'line' or link_type = 'cable');
   
 -- Scenario eGo data processing
@@ -51,10 +51,10 @@ INSERT INTO	scenario.eGo_data_processing_clean_run (version,schema_name,table_na
 		COUNT(line_id)AS entries,
 		'OK' AS status,
 		NOW() AT TIME ZONE 'Europe/Berlin' AS timestamp
-FROM	calc_ego_hv_powerflow.line;
+FROM	model_draft.ego_grid_pf_hv_line;
 
 -- TRANSFORMER DATA
-INSERT INTO calc_ego_hv_powerflow.transformer (trafo_id, bus0, bus1, x, s_nom, tap_ratio, phase_shift, geom, topo)
+INSERT INTO model_draft.ego_grid_pf_hv_transformer (trafo_id, bus0, bus1, x, s_nom, tap_ratio, phase_shift, geom, topo)
 SELECT 
   branch_id AS trafo_id,
   f_bus AS bus0,
@@ -65,7 +65,7 @@ SELECT
   shift AS phase_shift,
   geom,
   topo
-  FROM calc_ego_osmtgmod.branch_data
+  FROM grid.otg_ehvhv_branch_data
   WHERE result_id = 1 and link_type = 'transformer';
   
   
@@ -78,41 +78,41 @@ INSERT INTO	scenario.eGo_data_processing_clean_run (version,schema_name,table_na
 		COUNT(trafo_id)AS entries,
 		'OK' AS status,
 		NOW() AT TIME ZONE 'Europe/Berlin' AS timestamp
-FROM	calc_ego_hv_powerflow.transformer;
+FROM	model_draft.ego_grid_pf_hv_transformer;
 
 -- per unit to absolute values
 
-UPDATE calc_ego_hv_powerflow.line a
+UPDATE model_draft.ego_grid_pf_hv_line a
 	SET 
 		r = r * ((result.v_nom * 1000)^2 / (100 * 10^6)),
 		x = x * ((result.v_nom * 1000)^2 / (100 * 10^6)),
 		b = b * ((result.v_nom * 1000)^2 / (100 * 10^6)) 
 		FROM 
-			(SELECT bus_id, v_nom FROM calc_ego_hv_powerflow.bus)
+			(SELECT bus_id, v_nom FROM model_draft.ego_grid_pf_hv_bus)
 			as result
 WHERE a.bus0 = result.bus_id;
 
 
-UPDATE calc_ego_hv_powerflow.transformer a
+UPDATE model_draft.ego_grid_pf_hv_transformer a
 	SET 
 		x = x * ((result.v_nom * 1000)^2 / (100 * 10^6))
 		FROM 
 			(SELECT 
 			trafo_id, 
 			GREATEST(
-				(SELECT v_nom as v_nom_bus0 FROM calc_ego_hv_powerflow.bus WHERE bus_id = bus0), 
-				(SELECT v_nom as v_nom_bus1 FROM calc_ego_hv_powerflow.bus WHERE bus_id = bus1)) 
+				(SELECT v_nom as v_nom_bus0 FROM model_draft.ego_grid_pf_hv_bus WHERE bus_id = bus0), 
+				(SELECT v_nom as v_nom_bus1 FROM model_draft.ego_grid_pf_hv_bus WHERE bus_id = bus1)) 
 				as v_nom 
-			FROM calc_ego_hv_powerflow.transformer) as result
+			FROM model_draft.ego_grid_pf_hv_transformer) as result
 WHERE a.trafo_id = result.trafo_id;
 
 -- calculate line length (in km) from geoms
 
-UPDATE calc_ego_hv_powerflow.line a
+UPDATE model_draft.ego_grid_pf_hv_line a
 	SET 
 		length = result.length
 		FROM 
 		(SELECT b.line_id, st_length(b.geom,false)/1000 as length 
-		from calc_ego_hv_powerflow.line b)
+		from model_draft.ego_grid_pf_hv_line b)
 		as result
 WHERE a.line_id = result.line_id;
