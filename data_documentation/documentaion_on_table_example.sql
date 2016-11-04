e9e0c4e359c15c464838047e8550c381511ef351
Ludwig Hülk <ludwig.huelk@rl-institut.de>
2016-11-04 11:16:11 +0100

    metadata update ticket #23

--- data_documentation/documentaion_on_table_example.sql
+++ data_documentation/documentaion_on_table_example.sql
@@ -6,26 +6,30 @@ A further description can be found on http://wiki.openmod-initiative.org/wiki/Da
 
 -- metadata
 COMMENT ON TABLE schema.table IS '{
-    "Name": "Original name of the data set",
+    "Name": "Original name",
     "Source":   [{
         "Name": "Website of data",
         "URL":  "www.website.com" }],
     "Reference date": "2013",
     "Date of collection": "01.08.2013",
     "Original file": "346-22-5.xls",
-    "Spatial resolution": ["Germany"],
-    "Description": ["Example Data (annual totals)", "Regional level: national"],
+    "Spatial": [{
+	"Resolution": "100m x 100m",
+	"Extend": "Germany" }],
+    "Description": ["Example Data"],
     "Column":[
         {"Name": "id", "Description": "Unique identifier", "Unit": " " },
         {"Name": "year", "Description": "Reference Year", "Unit": " " },
-        {"Name": "example_value", "Description": "Some important value", "Unit": "EUR" } ],
+        {"Name": "example_value", "Description": "Price", "Unit": "EUR" } ],
     "Changes":	[
         {"Name": "Joe Nobody", "Mail": "joe.nobody@gmail.com (fake)",
         "Date":  "16.06.2014", "Comment": "Created table" },
         {"Name": "Joana Anybody", "Mail": "joana.anybody@gmail.com (fake)",
         "Date": "17.07.2014", "Comment": "Translated field names" } ],
-    "ToDo": ["Some datasets are odd -&gt; Check numbers against another data"],
-    "Licence": ["Licence – Version 2.0 (dl-de/by-2-0; [http://www.govdata.de/dl-de/by-2-0])"],
+    "Notes": ["Some datasets are odd -&gt; Check numbers against another data"],
+    "Licence": [{
+	"Name": "Licence – Version 2.0 (dl-de/by-2-0)", 
+	" URL": "http://www.govdata.de/dl-de/by-2-0" }],
     "Instructions for proper use": ["Always state licence"]
     }' ;
 
