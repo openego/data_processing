8979a2ba1496c3331351a3dd5eb51df8cf101e20
Ludwig Huelk <ludwig.huelk@rl-institut.de>
2016-11-22 14:33:33 +0100

    metadata template updated, harmonized with opsd and oknf guidelines

--- data_documentation/metadata_on_table_example.sql
+++ data_documentation/metadata_on_table_example.sql
@@ -6,32 +6,42 @@ A further description can be found on http://wiki.openmod-initiative.org/wiki/Da
 
 -- metadata
 COMMENT ON TABLE schema.table IS '{
-    "Name": "Original name",
-    "Source":   [{
-        "Name": "Website of data",
-        "URL":  "www.website.com" }],
-    "Reference date": "2013",
-    "Date of collection": "01.08.2013",
-    "Original file": "346-22-5.xls",
-    "Spatial": [{
-	"Resolution": "100m x 100m",
-	"Extend": "Germany" }],
-    "Description": ["Example Data"],
-    "Column":[
-        {"Name": "id", "Description": "Unique identifier", "Unit": " " },
-        {"Name": "year", "Description": "Reference Year", "Unit": " " },
-        {"Name": "example_value", "Description": "Price", "Unit": "EUR" } ],
-    "Changes":	[
-        {"Name": "Joe Nobody", "Mail": "joe.nobody@gmail.com (fake)",
-        "Date":  "16.06.2014", "Comment": "Created table" },
-        {"Name": "Joana Anybody", "Mail": "joana.anybody@gmail.com (fake)",
-        "Date": "17.07.2014", "Comment": "Translated field names" } ],
-    "Notes": ["Some datasets are odd -&gt; Check numbers against another data"],
-    "Licence": [{
-	"Name": "Licence – Version 2.0 (dl-de/by-2-0)", 
-	"URL": "http://www.govdata.de/dl-de/by-2-0" }],
-    "Instructions for proper use": ["Always state licence"]
-    }' ;
+	"title":"Good example title",
+	"description":"example metadata for example data",
+	"keywords":["example", "test"],
+	"sources": [{
+		"name":"Website",
+		"description":"Data website",
+		"url":"www.website.com"}],
+	"spatial": [{
+		"extend":"europe",
+		"resolution":"100mx100m"}],
+	"license": [{
+		"id":"GPL-3.0",
+		"name":"GNU General Public License 3.0",
+		"version":"3.0",
+		"url":"https://opensource.org/licenses/GPL-3.0",
+		"instruction":""}],
+	"contributors": [
+		{"name":"Ludwig Hülk",
+		"email":"ludwig.huelk@rl-institut.de",
+		"date":"16.06.2016",
+		"comment":"created metadata"},
+		{"name":"Ludwig Hülk",
+		"email":"ludwig.huelk@rl-institut.de",
+		"date":"22.11.2016",
+		"comment":"updated metadata"}],
+	"notes":"use this example",
+	"resources": [{
+		"schema": {
+			"fields": [
+				{"name":"id",
+				"description":"unique identifier",
+				"unit":"" },
+				{"name":"year",
+				"description":"reference year",
+				"unit":"" }
+				]  } }] }';
 
 -- select description
 SELECT obj_description('schema.table' ::regclass) ::json;
