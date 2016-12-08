cf3b29055dcc345f96e21179ad71cc638534f7f8
Ludwig Huelk <ludwig.huelk@rl-institut.de>
2016-12-08 16:02:55 +0100

    metadata finalization

--- data_documentation/metadata_on_table_example.sql
+++ data_documentation/metadata_on_table_example.sql
@@ -3,9 +3,8 @@ This script provides an SQL example of the metadata documentation
 A definition of metadata can be found in the openmod glossary http://wiki.openmod-initiative.org/wiki/Metadata
 A further description can be found on http://wiki.openmod-initiative.org/wiki/DatabaseRules
 
-Copyright (C) 2016  open_eGo project
-Published under GNU General Public License 3.0 (GPL-3.0)
-see https://github.com/openego/data_processing/blob/master/LICENSE
+__copyright__ = "Copyright open_eGo "
+__license__ = "GPLv3"
 */
 
 -- metadata
@@ -15,10 +14,8 @@ COMMENT ON TABLE schema.table IS '{
 	"language": [ "eng", "ger", "fre" ],
 	"reference_date": "2016-01-24",
 	"sources": [
-		{"name": "Website",
-		"url": "www.website.com"},
-		{"name": "Website",
-		"url": "www.website.com"} ],
+		{"name": "Website","description": "Data website","url": "www.website.com"},
+		{"name": "Website","description": "Data website","url": "www.website.com"} ],
 	"spatial": [
 		{"extend": "europe",
 		"resolution": "100mx100m"} ],
@@ -29,24 +26,17 @@ COMMENT ON TABLE schema.table IS '{
 		"url": "https://opensource.org/licenses/GPL-3.0",
 		"instruction": "You may copy, distribute and modify the software as long as you track changes/dates in source files. Any modifications to or software including (via compiler) GPL-licensed code must also be made available under the GPL along with build & install instructions."} ],
 	"contributors": [
-		{"name": "Ludwig Hülk",
-		"email": "ludwig.huelk@rl-institut.de",
-		"date": "16.06.2016",
-		"comment": "created metadata"},
-		{"name": "Ludwig Hülk",
-		"email": "ludwig.huelk@rl-institut.de",
-		"date": "22.11.2016",
-		"comment": "updated metadata"} ],
+		{"name": "Ludwig Hülk",	"email": "ludwig.huelk@rl-institut.de",
+		"date": "16.06.2016", "comment": "created metadata"},
+		{"name": "Ludwig Hülk", "email": "ludwig.huelk@rl-institut.de",
+		"date": "22.11.2016", "comment": "updated metadata"},
+		{"name": "Ludwig Hülk", "email": "ludwig.huelk@rl-institut.de",
+		"date": "22.11.2016", "comment": "updated header and license"} ],
 	"resources": [{
 		"schema": {
 			"fields": [
-				{"name": "id",
-				"description": "unique identifier",
-				"unit": "" },
-				{"name": "year",
-				"description": "reference year",
-				"unit": "" }
-				]  },
+				{"name": "id", "description": "unique identifier", "unit": "" },
+				{"name": "year", "description": "reference year", "unit": "" } ]},
 		"meta_version": "1.0"}] }';
 
 -- select description
