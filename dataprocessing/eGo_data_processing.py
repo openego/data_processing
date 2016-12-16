6ae4053a741284579f00fd95126d4f2bf5b61556
Ludwig Huelk <ludwig.huelk@rl-institut.de>
2016-12-16 15:11:46 +0100

    ego scenario function

--- dataprocessing/eGo_data_processing.py
+++ dataprocessing/eGo_data_processing.py
@@ -41,7 +41,7 @@ def data_processing():
     snippets = [
         # 'ego_scenario_log_setup.sql',				# setup scenario log table
         # 'ego_boundaries_vg250_setup.sql',			# setup borders
-        # 'get_substations.sql',                      # setup hvmv substaions
+        'get_substations.sql',                      # setup hvmv substaions
         # 'get_substations_ehv.sql',                # setup ehv substaions
         # 'otg_id_to_substations.sql',
         # 'ego_grid_hvmv_substation.sql',				# hvmv substation vornoi
