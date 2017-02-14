479dfc3d588e18b15c42028892e44197e0de859c
nesnoj <jonathan.amme@rl-institut.de>
2017-02-14 11:53:39 +0100

    use latest ego.io table defs

--- dataprocessing/python_scripts/setup_ego_mv_powerflow.py
+++ dataprocessing/python_scripts/setup_ego_mv_powerflow.py
@@ -72,25 +72,24 @@ def create_powerflow_schema(engine, schema, tables):
     # Bus.metadata.drop_all(engine)
 
 
-
 if __name__ == '__main__':
     """Create or delete powerflow schema"""
 
     engine = db.engine(section='oedb')
 
-    schema = 'calc_ego_mv_powerflow'
+    schema = 'model_draft'
 
     # tables = [Bus, Line, Generator, Load, Storage, Source]
 
     tables = {
-        'bus': ['bus_id character varying(25) NOT NULL', 'v_nom double precision',
+        'ego_grid_pf_mv_bus': ['bus_id character varying(25) NOT NULL', 'v_nom double precision',
                 'v_mag_pu_min double precision DEFAULT 0',
                 'v_mag_pu_max double precision',
                 'geom geometry(Point,4326)',
                 "scn_name character varying NOT NULL DEFAULT 'Status Quo'::character varying",
                 "grid_id integer",
                 'CONSTRAINT bus_data_pkey PRIMARY KEY (bus_id, scn_name)'],
-        'generator': ["scn_name character varying NOT NULL DEFAULT 'Status Quo'::character varying",
+        'ego_grid_pf_mv_generator': ["scn_name character varying NOT NULL DEFAULT 'Status Quo'::character varying",
             'generator_id character varying(25) NOT NULL', 'bus character varying(25)',
             "control text DEFAULT 'PQ'::text",
             'p_nom double precision DEFAULT 0',
@@ -99,7 +98,7 @@ if __name__ == '__main__':
             'sign double precision DEFAULT 1',
             "grid_id integer",
             'CONSTRAINT generator_data_pkey PRIMARY KEY (generator_id, scn_name)'],
-        'line': ["scn_name character varying NOT NULL DEFAULT 'Status Quo'::character varying",
+        'ego_grid_pf_mv_line': ["scn_name character varying NOT NULL DEFAULT 'Status Quo'::character varying",
             'line_id character varying(25) NOT NULL', 'bus0 character varying(25)', 'bus1 character varying(25)',
             'x numeric DEFAULT 0',
             'r numeric DEFAULT 0',
@@ -111,19 +110,19 @@ if __name__ == '__main__':
             'geom geometry(LineString,4326)',
             "grid_id integer",
             'CONSTRAINT line_data_pkey PRIMARY KEY (line_id, scn_name)'],
-        'load': ["scn_name character varying NOT NULL DEFAULT 'Status Quo'::character varying",
+        'ego_grid_pf_mv_load': ["scn_name character varying NOT NULL DEFAULT 'Status Quo'::character varying",
             "load_id character varying(25) NOT NULL",
             'bus character varying(25)',
             'sign double precision DEFAULT (-1)',
             "grid_id integer",
             'CONSTRAINT load_data_pkey PRIMARY KEY (load_id, scn_name)'],
-        'temp_resolution': ['temp_id bigint NOT NULL',
+        'ego_grid_pf_mv_temp_resolution': ['temp_id bigint NOT NULL',
             'timesteps bigint NOT NULL',
             'resolution text',
             'start_time timestamp without time zone', # style: YYYY-MM-DD HH:MM:SS
             "grid_id integer",
             'CONSTRAINT temp_resolution_pkey PRIMARY KEY (temp_id)'],
-        'transformer': ["scn_name character varying NOT NULL DEFAULT 'Status Quo'::character varying",
+        'ego_grid_pf_mv_transformer': ["scn_name character varying NOT NULL DEFAULT 'Status Quo'::character varying",
             'trafo_id character varying(25) NOT NULL',
             'bus0 character varying(25)',
             'bus1 character varying(25)',
@@ -136,7 +135,7 @@ if __name__ == '__main__':
             'geom geometry(Point,4326)',
             "grid_id integer",
             'CONSTRAINT transformer_data_pkey PRIMARY KEY (trafo_id, scn_name)'],
-        'scenario_settings': ["scn_name character varying NOT NULL DEFAULT 'Status Quo'::character varying",
+        'ego_grid_pf_mv_scenario_settings': ["scn_name character varying NOT NULL DEFAULT 'Status Quo'::character varying",
             'bus character varying',
             'bus_v_mag_set character varying',
             'generator character varying',
@@ -149,14 +148,14 @@ if __name__ == '__main__':
             'temp_resolution character varying',
             'transformer character varying',
             'CONSTRAINT scenario_settings_pkey PRIMARY KEY (scn_name)'],
-        'bus_v_mag_set': ["scn_name character varying NOT NULL DEFAULT 'Status Quo'::character varying",
+        'ego_grid_pf_mv_bus_v_mag_set': ["scn_name character varying NOT NULL DEFAULT 'Status Quo'::character varying",
               'bus_id character varying(25) NOT NULL',
               'temp_id integer NOT NULL',
               'v_mag_pu_set double precision[]',
                 "grid_id integer",
               'CONSTRAINT bus_v_mag_set_pkey PRIMARY KEY (bus_id, temp_id, scn_name)'],
               # 'CONSTRAINT bus_v_mag_set_temp_fkey FOREIGN KEY (temp_id) REFERENCES calc_ego_hv_powerflow.temp_resolution (temp_id)'],
-        'generator_pq_set': ["scn_name character varying NOT NULL DEFAULT 'Status Quo'::character varying",
+        'ego_grid_pf_mv_generator_pq_set': ["scn_name character varying NOT NULL DEFAULT 'Status Quo'::character varying",
             'generator_id character varying(25) NOT NULL',
             'temp_id integer NOT NULL',
             'p_set double precision[]',
@@ -166,7 +165,7 @@ if __name__ == '__main__':
             "grid_id integer",
             'CONSTRAINT generator_pq_set_pkey PRIMARY KEY (generator_id, temp_id, scn_name)'],
             # 'CONSTRAINT generator_pq_set_temp_fkey FOREIGN KEY (temp_id) REFERENCES calc_ego_hv_powerflow.temp_resolution (temp_id)'],
-        'load_pq_set': ["scn_name character varying NOT NULL "
+        'ego_grid_pf_mv_load_pq_set': ["scn_name character varying NOT NULL "
                           "DEFAULT 'Status Quo'::character varying",
             'load_id character varying(25) NOT NULL',
             'temp_id integer NOT NULL',
