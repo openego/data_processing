d3629de1ba4bc84c3bbdee1b2ffbb4141bf0d2a9
nesnoj <jonathan.amme@rl-institut.de>
2017-02-14 12:53:25 +0100

    add logging, fix pkeys

--- dataprocessing/python_scripts/setup_ego_mv_powerflow.py
+++ dataprocessing/python_scripts/setup_ego_mv_powerflow.py
@@ -9,6 +9,7 @@ __author__ = "gplssm"
 
 from oemof import db
 from oemof.db import tools
+from dataprocessing.python_scripts.functions.ego_scenario_log import write_ego_scenario_log
 
 
 def create_powerflow_schema(engine, schema, tables):
@@ -25,33 +26,33 @@ def create_powerflow_schema(engine, schema, tables):
 
     group = 'oeuser'
 
-    try:
-        # create schema
-        sql_create_schema = '''CREATE SCHEMA {0} AUTHORIZATION {1};
-            '''.format(schema, group)
-
-        conn.execute(sql_create_schema)
-
-        # grant acess rights for schema
-        tools.grant_schema(conn, schema, group)
-
-        # alter privileges for schema
-        sql_privileges_tables = '''ALTER DEFAULT PRIVILEGES IN SCHEMA {0}
-            GRANT INSERT, SELECT, UPDATE, DELETE, TRUNCATE, REFERENCES, TRIGGER
-            ON TABLES TO {1};'''.format(schema, group)
-        conn.execute(sql_privileges_tables)
-
-        sql_privileges_sequences = '''ALTER DEFAULT PRIVILEGES IN SCHEMA {0}
-            GRANT SELECT, UPDATE, USAGE ON SEQUENCES TO {1};
-            '''.format(schema, group)
-        conn.execute(sql_privileges_sequences)
-
-        sql_privileges_functions = '''ALTER  DEFAULT PRIVILEGES IN SCHEMA {0}
-             GRANT EXECUTE ON FUNCTIONS TO {1};'''
-        conn.execute(sql_privileges_functions)
-    except:
-        print('Schema {} already existed and is not created newly...'\
-              .format(schema))
+    # try:
+    #     # create schema
+    #     sql_create_schema = '''CREATE SCHEMA {0} AUTHORIZATION {1};
+    #         '''.format(schema, group)
+    #
+    #     conn.execute(sql_create_schema)
+    #
+    #     # grant acess rights for schema
+    #     tools.grant_schema(conn, schema, group)
+    #
+    #     # alter privileges for schema
+    #     sql_privileges_tables = '''ALTER DEFAULT PRIVILEGES IN SCHEMA {0}
+    #         GRANT INSERT, SELECT, UPDATE, DELETE, TRUNCATE, REFERENCES, TRIGGER
+    #         ON TABLES TO {1};'''.format(schema, group)
+    #     conn.execute(sql_privileges_tables)
+    #
+    #     sql_privileges_sequences = '''ALTER DEFAULT PRIVILEGES IN SCHEMA {0}
+    #         GRANT SELECT, UPDATE, USAGE ON SEQUENCES TO {1};
+    #         '''.format(schema, group)
+    #     conn.execute(sql_privileges_sequences)
+    #
+    #     sql_privileges_functions = '''ALTER  DEFAULT PRIVILEGES IN SCHEMA {0}
+    #          GRANT EXECUTE ON FUNCTIONS TO {1};'''
+    #     conn.execute(sql_privileges_functions)
+    # except:
+    #     print('Schema {} already existed and is not created newly...'\
+    #           .format(schema))
 
 
     # iterate over tables and create them
@@ -67,6 +68,14 @@ def create_powerflow_schema(engine, schema, tables):
         #     print('Table {} already existed and is not created newly...'\
         #       .format(table))
         tools.grant_db_access(conn, schema, table, group)
+
+        write_ego_scenario_log(conn=conn,
+                               version='v0.2.6',
+                               io='output',
+                               schema=schema,
+                               table=table,
+                               script='setup_ego_mv_powerflow.py',
+                               comment='create table')
 
 
     # Bus.metadata.drop_all(engine)
@@ -88,7 +97,7 @@ if __name__ == '__main__':
                 'geom geometry(Point,4326)',
                 "scn_name character varying NOT NULL DEFAULT 'Status Quo'::character varying",
                 "grid_id integer",
-                'CONSTRAINT bus_data_pkey PRIMARY KEY (bus_id, scn_name)'],
+                'CONSTRAINT ego_grid_pf_mv_bus_pkey PRIMARY KEY (bus_id, scn_name)'],
         'ego_grid_pf_mv_generator': ["scn_name character varying NOT NULL DEFAULT 'Status Quo'::character varying",
             'generator_id character varying(25) NOT NULL', 'bus character varying(25)',
             "control text DEFAULT 'PQ'::text",
@@ -97,7 +106,7 @@ if __name__ == '__main__':
             'p_max_pu_fixed double precision DEFAULT 1',
             'sign double precision DEFAULT 1',
             "grid_id integer",
-            'CONSTRAINT generator_data_pkey PRIMARY KEY (generator_id, scn_name)'],
+            'CONSTRAINT ego_grid_pf_mv_generator_pkey PRIMARY KEY (generator_id, scn_name)'],
         'ego_grid_pf_mv_line': ["scn_name character varying NOT NULL DEFAULT 'Status Quo'::character varying",
             'line_id character varying(25) NOT NULL', 'bus0 character varying(25)', 'bus1 character varying(25)',
             'x numeric DEFAULT 0',
@@ -109,19 +118,19 @@ if __name__ == '__main__':
             'cables integer',
             'geom geometry(LineString,4326)',
             "grid_id integer",
-            'CONSTRAINT line_data_pkey PRIMARY KEY (line_id, scn_name)'],
+            'CONSTRAINT ego_grid_pf_mv_line_pkey PRIMARY KEY (line_id, scn_name)'],
         'ego_grid_pf_mv_load': ["scn_name character varying NOT NULL DEFAULT 'Status Quo'::character varying",
             "load_id character varying(25) NOT NULL",
             'bus character varying(25)',
             'sign double precision DEFAULT (-1)',
             "grid_id integer",
-            'CONSTRAINT load_data_pkey PRIMARY KEY (load_id, scn_name)'],
+            'CONSTRAINT ego_grid_pf_mv_load_pkey PRIMARY KEY (load_id, scn_name)'],
         'ego_grid_pf_mv_temp_resolution': ['temp_id bigint NOT NULL',
             'timesteps bigint NOT NULL',
             'resolution text',
             'start_time timestamp without time zone', # style: YYYY-MM-DD HH:MM:SS
             "grid_id integer",
-            'CONSTRAINT temp_resolution_pkey PRIMARY KEY (temp_id)'],
+            'CONSTRAINT ego_grid_pf_mv_temp_resolution_pkey PRIMARY KEY (temp_id)'],
         'ego_grid_pf_mv_transformer': ["scn_name character varying NOT NULL DEFAULT 'Status Quo'::character varying",
             'trafo_id character varying(25) NOT NULL',
             'bus0 character varying(25)',
@@ -134,7 +143,7 @@ if __name__ == '__main__':
             'tap_ratio double precision',
             'geom geometry(Point,4326)',
             "grid_id integer",
-            'CONSTRAINT transformer_data_pkey PRIMARY KEY (trafo_id, scn_name)'],
+            'CONSTRAINT ego_grid_pf_mv_transformer_pkey PRIMARY KEY (trafo_id, scn_name)'],
         'ego_grid_pf_mv_scenario_settings': ["scn_name character varying NOT NULL DEFAULT 'Status Quo'::character varying",
             'bus character varying',
             'bus_v_mag_set character varying',
@@ -147,13 +156,13 @@ if __name__ == '__main__':
             'storage_pq_set character varying',
             'temp_resolution character varying',
             'transformer character varying',
-            'CONSTRAINT scenario_settings_pkey PRIMARY KEY (scn_name)'],
+            'CONSTRAINT ego_grid_pf_mv_scenario_settings_pkey PRIMARY KEY (scn_name)'],
         'ego_grid_pf_mv_bus_v_mag_set': ["scn_name character varying NOT NULL DEFAULT 'Status Quo'::character varying",
               'bus_id character varying(25) NOT NULL',
               'temp_id integer NOT NULL',
               'v_mag_pu_set double precision[]',
                 "grid_id integer",
-              'CONSTRAINT bus_v_mag_set_pkey PRIMARY KEY (bus_id, temp_id, scn_name)'],
+              'CONSTRAINT ego_grid_pf_mv_bus_v_mag_set_pkey PRIMARY KEY (bus_id, temp_id, scn_name)'],
               # 'CONSTRAINT bus_v_mag_set_temp_fkey FOREIGN KEY (temp_id) REFERENCES calc_ego_hv_powerflow.temp_resolution (temp_id)'],
         'ego_grid_pf_mv_generator_pq_set': ["scn_name character varying NOT NULL DEFAULT 'Status Quo'::character varying",
             'generator_id character varying(25) NOT NULL',
@@ -163,7 +172,7 @@ if __name__ == '__main__':
             'p_min_pu double precision[]',
             'p_max_pu double precision[]',
             "grid_id integer",
-            'CONSTRAINT generator_pq_set_pkey PRIMARY KEY (generator_id, temp_id, scn_name)'],
+            'CONSTRAINT ego_grid_pf_mv_generator_pq_set_pkey PRIMARY KEY (generator_id, temp_id, scn_name)'],
             # 'CONSTRAINT generator_pq_set_temp_fkey FOREIGN KEY (temp_id) REFERENCES calc_ego_hv_powerflow.temp_resolution (temp_id)'],
         'ego_grid_pf_mv_load_pq_set': ["scn_name character varying NOT NULL "
                           "DEFAULT 'Status Quo'::character varying",
@@ -172,8 +181,7 @@ if __name__ == '__main__':
             'p_set double precision[]',
             'q_set double precision[]',
             "grid_id integer",
-            'CONSTRAINT load_pq_set_pkey '
-            'PRIMARY KEY (load_id, temp_id, scn_name)'],
+            'CONSTRAINT ego_grid_pf_mv_load_pq_set_pkey PRIMARY KEY (load_id, temp_id, scn_name)'],
             # 'CONSTRAINT load_pq_set_temp_fkey FOREIGN KEY (temp_id) '
             # 'REFERENCES calc_ego_hv_powerflow.temp_resolution (temp_id)']
     }
