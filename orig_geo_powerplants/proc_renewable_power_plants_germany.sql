0d3512b45ec49b84255e6c53dc2dc887e26b383d
IlkaCu <ilka.cussmann@fh-flensburg.de>
2017-02-16 10:13:34 +0100

    Adjust all RE PP according to allocation table

--- orig_geo_powerplants/proc_renewable_power_plants_germany.sql
+++ orig_geo_powerplants/proc_renewable_power_plants_germany.sql
@@ -12,60 +12,31 @@ ALTER TABLE orig_geo_powerplants.proc_renewable_power_plants_germany
 ADD COLUMN voltage character varying;
 
 
-UPDATE orig_geo_powerplants.proc_renewable_power_plants_germany
-SET voltage=110,
-voltage_level='3'
-WHERE voltage_level='03 (HS)'  /*Voltage_level =3 when capacity between 15 and 119.9 and no entry in voltage_level*/;
-
-UPDATE orig_geo_powerplants.proc_renewable_power_plants_germany
-SET voltage_level='5'
-WHERE voltage_level='05 (MS)'  ;
-
-UPDATE orig_geo_powerplants.proc_renewable_power_plants_germany
-SET voltage_level='7'
-WHERE voltage_level='07 (NS)'  ;
-
-UPDATE orig_geo_powerplants.proc_renewable_power_plants_germany
-SET voltage_level='6'
-WHERE voltage_level='06 (MS/NS)'  ;
-
-UPDATE orig_geo_powerplants.proc_renewable_power_plants_germany
-SET voltage_level='4'
-WHERE voltage_level='04 (HS/MS)' ;
-
-UPDATE orig_geo_powerplants.proc_renewable_power_plants_germany
-SET voltage_level='2'
-WHERE voltage_level='02 (HöS/HS)' ;
-
-UPDATE orig_geo_powerplants.proc_renewable_power_plants_germany
-SET voltage_level='1'
-WHERE voltage_level='01 (HöS)' ;
-
-/* Update RE-plants with missing entry on voltage_level*/
+/* Adjust voltage level of all RE power plants according to allocation table*/
 UPDATE orig_geo_powerplants.proc_renewable_power_plants_germany 
 SET voltage_level='1'
-WHERE electrical_capacity >=120000 and voltage_level is NULL  ;
+WHERE electrical_capacity >=120000;
 
 UPDATE orig_geo_powerplants.proc_renewable_power_plants_germany
 SET voltage_level='3',
 Voltage=110
-WHERE electrical_capacity between 17500 and 119999.99 and voltage_level is NULL  ;
+WHERE electrical_capacity between 17500 and 119999.99;
 
 UPDATE orig_geo_powerplants.proc_renewable_power_plants_germany
 SET voltage_level='4'
-WHERE electrical_capacity between 4500 and 17499.99 and voltage_level is NULL  ;
+WHERE electrical_capacity between 4500 and 17499.99;
 
 UPDATE orig_geo_powerplants.proc_renewable_power_plants_germany
 SET voltage_level='5'
-WHERE electrical_capacity between 300 and 4499.99 and voltage_level is NULL  ;
+WHERE electrical_capacity between 300 and 4499.99;
 
 UPDATE orig_geo_powerplants.proc_renewable_power_plants_germany
 SET voltage_level='6'
-WHERE electrical_capacity between 100 and 299.99 and voltage_level is NULL  ;
+WHERE electrical_capacity between 100 and 299.99;
 
 UPDATE orig_geo_powerplants.proc_renewable_power_plants_germany
 SET voltage_level='7'
-WHERE electrical_capacity <100 and voltage_level is NULL  ;
+WHERE electrical_capacity <100;
 
 /*Assign RE-plants in ehv-level to nearest substation*/
 
