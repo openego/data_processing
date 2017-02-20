7aca6eeb955038895698c56e3dcbeebdfa21c014
IlkaCu <ilka.cussmann@fh-flensburg.de>
2017-02-20 14:23:23 +0100

    Adapt table names to new db structure

--- orig_geo_powerplants/proc_power_plant_germany.sql
+++ orig_geo_powerplants/proc_power_plant_germany.sql
@@ -3,13 +3,13 @@
 ------------
 
 
-CREATE TABLE orig_geo_powerplants.proc_power_plant_germany AS
-SELECT opsd_power_plants_germany.*
-FROM orig_geo_opsd.opsd_power_plants_germany
-;
+--CREATE TABLE orig_geo_powerplants.proc_power_plant_germany AS
+--SELECT opsd_power_plants_germany.*
+--FROM orig_geo_opsd.opsd_power_plants_germany
+--;
 
 
-COMMENT ON TABLE  orig_geo_powerplants.proc_power_plant_germany IS
+COMMENT ON TABLE  supply.ego_conv_powerplant IS
 '{
 "Name": "eGo conventional powerplant list",
 "Source": [{
@@ -157,48 +157,48 @@ COMMENT ON TABLE  orig_geo_powerplants.proc_power_plant_germany IS
 
 
 /* Manual modifications which are valid for version 1.0 of opsd_power_plants_germany. A validity for later versions is not quaranteed*/
-UPDATE orig_geo_powerplants.proc_power_plant_germany
+UPDATE supply.ego_conv_powerplant
 SET voltage='10;110'
 WHERE voltage='10 und 110'
 ;
 
-UPDATE orig_geo_powerplants.proc_power_plant_germany
+UPDATE supply.ego_conv_powerplant
 SET voltage='110;220'
 WHERE voltage='220 / 110 / 10'
 ;
 
-UPDATE orig_geo_powerplants.proc_power_plant_germany
+UPDATE supply.ego_conv_powerplant
 SET voltage='220'
 WHERE voltage='Werknetz' AND city = 'Salzgitter'
 ;
 
-UPDATE orig_geo_powerplants.proc_power_plant_germany
+UPDATE supply.ego_conv_powerplant
 SET voltage='MS'
 WHERE voltage='MSP'
 ;
 
-UPDATE orig_geo_powerplants.proc_power_plant_germany
+UPDATE supply.ego_conv_powerplant
 SET voltage='6;30'
 WHERE voltage='30 auf 6'
 ;
 
-UPDATE orig_geo_powerplants.proc_power_plant_germany
+UPDATE supply.ego_conv_powerplant
 SET voltage='110'
 WHERE voltage='Mai 25'
 ;
 
-UPDATE orig_geo_powerplants.proc_power_plant_germany
+UPDATE supply.ego_conv_powerplant
 SET voltage='10;25'
 WHERE voltage='10kV, 25kV'
 ;
 
-UPDATE orig_geo_powerplants.proc_power_plant_germany
+UPDATE supply.ego_conv_powerplant
 SET voltage='6'
 WHERE voltage='6
 20'
 ;
 
-UPDATE orig_geo_powerplants.proc_power_plant_germany
+UPDATE supply.ego_conv_powerplant
 SET voltage='6;110'
 WHERE voltage='110/6'
 ;
@@ -206,47 +206,47 @@ WHERE voltage='110/6'
 
 /*Correct an invalide geom in the register*/
 
-ALTER TABLE orig_geo_powerplants.proc_power_plant_germany
+ALTER TABLE supply.ego_conv_powerplant
 SET lat = 48.0261021
 WHERE gid = 493
 ;
 
-UPDATE  orig_geo_powerplants.proc_power_plant_germany
+UPDATE  supply.ego_conv_powerplant
 set geom = ST_SetSRID(ST_MakePoint(lon,lat),4326)
 WHERE gid = 493;
 
 /*Update Voltage Level of Power Plants in proc_power_plants_germany*/
-ALTER TABLE orig_geo_powerplants.proc_power_plant_germany
+ALTER TABLE supply.ego_conv_powerplant
 ADD COLUMN voltage_level smallint DEFAULT NULL
 ;
 
 
 /*Update Voltage Level of Power Plants in proc_power_plants_germany*/
 
-UPDATE orig_geo_powerplants.proc_power_plant_germany
+UPDATE supply.ego_conv_powerplant
 SET voltage_level=1
 WHERE capacity >=120.0 /*Voltage_level =1 when capacity greater than 120 MW*/;
 
 
-UPDATE orig_geo_powerplants.proc_power_plant_germany
+UPDATE supply.ego_conv_powerplant
 SET voltage_level=3
 WHERE capacity BETWEEN 17.5 AND 119.99 /*Voltage_level =2 when capacity between 17.5 and 119.99 MW*/;
 ;
-UPDATE orig_geo_powerplants.proc_power_plant_germany
+UPDATE supply.ego_conv_powerplant
 SET voltage_level=4
 WHERE capacity BETWEEN 4.5 AND 17.49
 ;
-UPDATE orig_geo_powerplants.proc_power_plant_germany
+UPDATE supply.ego_conv_powerplant
 SET voltage_level=5
 WHERE capacity BETWEEN 0.3 AND 4.49 /* Voltage_level =3 when capacity between 0.3 and 4.5 kV*/;
 ;
 
-UPDATE orig_geo_powerplants.proc_power_plant_germany
+UPDATE supply.ego_conv_powerplant
 SET voltage_level=6
 WHERE capacity BETWEEN 0.1 AND 0.29
 ;
 
-UPDATE orig_geo_powerplants.proc_power_plant_germany
+UPDATE supply.ego_conv_powerplant
 SET voltage_level=7
 WHERE capacity BETWEEN 0 AND 0.099 /*voltage_level =1 when capacity lower than 0.1*/
 ;
@@ -254,7 +254,7 @@ WHERE capacity BETWEEN 0 AND 0.099 /*voltage_level =1 when capacity lower than 0
 
 /* Change fuel='multiple_non_renewable' to 'other_non_renewable' for compatibility reasons*/
 
-UPDATE orig_geo_powerplants.proc_power_plant_germany
+UPDATE supply.ego_conv_powerplant
 	SET fuel = 'other_non_renewable'
 	WHERE fuel = 'multiple_non_renewable';
 
