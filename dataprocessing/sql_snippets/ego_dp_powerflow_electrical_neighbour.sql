59cf88377dfa4033b88d7887cfaca9e26ef859bf
IlkaCu <ilka.cussmann@fh-flensburg.de>
2017-03-07 13:42:07 +0100

    Include implementation of electrical neighbours in pf tables

--- dataprocessing/sql_snippets/ego_dp_powerflow_electrical_neighbour.sql
+++ dataprocessing/sql_snippets/ego_dp_powerflow_electrical_neighbour.sql
@@ -553,3 +553,14 @@ SET x = (CASE s_nom 	WHEN 1200 THEN 4.84
 			END);
 
 DELETE FROM model_draft.ego_grid_hv_electrical_neighbours_transformer WHERE x IS NULL;
+
+
+-- Include border crossing lines, transformer and buses for neighbouring states (electrical neighbours) for Status Quo
+INSERT INTO model_draft.ego_grid_pf_hv_line (scn_name, line_id, bus0, bus1, x, r, s_nom, topo, geom, length, frequency, cables)
+SELECT 'Status Quo', line_id, bus0, bus1, x, r, s_nom, topo, geom, length, frequency, cables FROM model_draft.ego_grid_hv_electrical_neighbours_line;
+
+INSERT INTO model_draft.ego_grid_pf_hv_bus (scn_name, bus_id, v_nom, geom)
+SELECT 'Status Quo',bus_id, v_nom, geom FROM model_draft.ego_grid_hv_electrical_neighbours_bus WHERE id < 28;
+
+INSERT INTO model_draft.ego_grid_pf_hv_transformer (scn_name, trafo_id, bus0, bus1, x, s_nom, geom)
+SELECT 'Status Quo', trafo_id, bus0, bus1, x, s_nom, geom FROM model_draft.ego_grid_hv_electrical_neighbours_transformer
