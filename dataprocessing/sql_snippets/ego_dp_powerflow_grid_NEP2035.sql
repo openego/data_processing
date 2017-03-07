536017b41b8f1b395d6d6a9c0b14440563a585b2
IlkaCu <ilka.cussmann@fh-flensburg.de>
2017-03-07 15:36:05 +0100

    Adjust table names

--- dataprocessing/sql_snippets/ego_dp_powerflow_grid_NEP2035.sql
+++ dataprocessing/sql_snippets/ego_dp_powerflow_grid_NEP2035.sql
@@ -13,31 +13,31 @@ __author__ 	= "IlkaCu"
 
 -- Use buses from the scenario 'Status Quo' for the new scenario
 
-DELETE FROM calc_ego_hv_powerflow.bus WHERE scn_name = 'NEP 2035'; 
+DELETE FROM model_draft.ego_grid_pf_hv_bus WHERE scn_name = 'NEP 2035'; 
 
-INSERT INTO calc_ego_hv_powerflow.bus
+INSERT INTO model_draft.ego_grid_pf_hv_bus
 SELECT 'NEP 2035', a.bus_id, a.v_nom, a.current_type, a.v_mag_pu_min, a.v_mag_pu_max, a.geom
-FROM 	calc_ego_hv_powerflow.bus a 
+FROM 	model_draft.ego_grid_pf_hv_bus a 
 WHERE scn_name= 'Status Quo';
 
 
 -- Include all lines from SQ scenario in the NEP 2035 scenario
 
-DELETE FROM calc_ego_hv_powerflow.line WHERE scn_name = 'NEP 2035'; 
+DELETE FROM model_draft.ego_grid_pf_hv_line WHERE scn_name = 'NEP 2035'; 
 
-INSERT INTO 	calc_ego_hv_powerflow.line
+INSERT INTO 	model_draft.ego_grid_pf_hv_line
 SELECT 'NEP 2035', a.line_id, a.bus0, a.bus1, a.x, a.r, a.g, a.b, a.s_nom, a.s_nom_extendable, a.s_nom_min, 
 	a.s_nom_max, a.capital_cost, a.length, a.cables, a.frequency, a.terrain_factor, a.geom, a.topo
-FROM 	calc_ego_hv_powerflow.line a 
+FROM 	model_draft.ego_grid_pf_hv_line a 
 WHERE 	scn_name= 'Status Quo'; 
 
 
 -- Include transformers from Status Quo into new scenario 'NEP 2035'
 
-DELETE FROM calc_ego_hv_powerflow.transformer WHERE scn_name = 'NEP 2035'; 
+DELETE FROM model_draft.ego_grid_pf_hv_transformer WHERE scn_name = 'NEP 2035'; 
 
-INSERT INTO calc_ego_hv_powerflow.transformer
+INSERT INTO model_draft.ego_grid_pf_hv_transformer
 SELECT 'NEP 2035', a.trafo_id, a.bus0, a.bus1, a.x, a.r, a.g, a.b, a.s_nom, a.s_nom_extendable, a.s_nom_min, 
 	a.s_nom_max, a.tap_ratio, a.phase_shift, a.capital_cost, a.geom, a.topo
-FROM 	calc_ego_hv_powerflow.transformer a 
-WHERE scn_name= 'Status Quo'; 
+FROM 	model_draft.ego_grid_pf_hv_transformer a 
+WHERE scn_name= 'Status Quo';
