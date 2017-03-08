91b4b7e719e0f84faa6adfd46900a5b57efcd56d
ClaraBuettner <clarabuettner@aol.com>
2017-03-08 09:18:43 +0100

    Update ego_dp_powerflow_electrical_neighbour.sql
    Schweden wird über den vorherigen AC-Punkt in Deutschland angeschlossen

--- dataprocessing/sql_snippets/ego_dp_powerflow_electrical_neighbour.sql
+++ dataprocessing/sql_snippets/ego_dp_powerflow_electrical_neighbour.sql
@@ -213,14 +213,19 @@ INSERT INTO model_draft.ego_grid_hv_electrical_neighbours_bus (bus_id, v_nom, ge
 		27);
 
 
-INSERT INTO model_draft.ego_grid_hv_electrical_neighbours_bus (bus_id, v_nom, geom, cntr_id, id)
+INSERT INTO electrical_neighbours.bus (bus_id, v_nom, geom, cntr_id, id)
 	
 SELECT bus_i, base_kv, geom, cntr_id, bus_i
-FROM grid.otg_ehvhv_bus_data;
+FROM grid.otg_ehvhv_bus_data WHERE frequency = 50;
 
 
-DELETE FROM model_draft.ego_grid_hv_electrical_neighbours_bus WHERE cntr_id = 'DE' OR cntr_id = '';
-DELETE FROM model_draft.ego_grid_hv_electrical_neighbours_bus WHERE cntr_id IS NULL;
+DELETE FROM electrical_neighbours.bus WHERE cntr_id = 'DE';
+DELETE FROM electrical_neighbours.bus WHERE cntr_id IS NULL;
+
+
+INSERT INTO electrical_neighbours.bus (bus_id, v_nom, geom, cntr_id, id)
+SELECT bus_i, base_kv, geom, 'SE', bus_i 
+FROM grid.otg_ehvhv_bus_data WHERE (geom = '0101000020E6100000A444C3ABCE9A254079A450D5E2F24A40' AND base_kv = 380);
 
 
 
