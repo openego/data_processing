f38e327b1bf8dd6461c7ef1a2d01deda6b63573d
Ludwig Huelk <ludwig.huelk@rl-institut.de>
2017-03-16 11:16:14 +0100

    update metadata to version 1.1

--- documentation/metadata_on_table_example.sql
+++ documentation/metadata_on_table_example.sql
@@ -7,6 +7,7 @@ __copyright__ 	= "Reiner Lemoine Institut gGmbH"
 __license__ 	= "GNU Affero General Public License Version 3 (AGPL-3.0)"
 __url__ 	= "https://github.com/openego/data_processing/blob/master/LICENSE"
 __author__ 	= "Ludee"
+__contains__	= "http://stackoverflow.com/questions/383692/what-is-json-and-why-would-i-use-it"
 */
 
 -- metadata
@@ -16,11 +17,13 @@ COMMENT ON TABLE schema.table IS '{
 	"language": [ "eng", "ger", "fre" ],
 	"reference_date": "2016-01-24",
 	"sources": [
-		{"name": "Website","description": "Data website","url": "www.website.com"},
-		{"name": "Website","description": "Data website","url": "www.website.com"} ],
+		{"name": "Website", "description": "Data website",
+		"url": "www.website.com", "license": "ODbL-1.0"},
+		{"name": "Website", "description": "Data website",
+		"url": "www.website.com", "license": "ODbL-1.0"} ],
 	"spatial": [
 		{"extend": "europe",
-		"resolution": "100mx100m"} ],
+		"resolution": "100m"} ],
 	"license": [
 		{"id": "tba",
 		"name": "tba",
@@ -28,18 +31,20 @@ COMMENT ON TABLE schema.table IS '{
 		"url": "tba",
 		"instruction": "tba"} ],
 	"contributors": [
-		{"name": "Ludwig Hülk",	"email": "ludwig.huelk@rl-institut.de",
+		{"name": "Ludee", "email": " ",
 		"date": "16.06.2016", "comment": "created metadata"},
-		{"name": "Ludwig Hülk", "email": "ludwig.huelk@rl-institut.de",
+		{"name": "Ludee", "email": " ",
 		"date": "22.11.2016", "comment": "updated metadata"},
-		{"name": "Ludwig Hülk", "email": "ludwig.huelk@rl-institut.de",
-		"date": "22.11.2016", "comment": "updated header and license"} ],
+		{"name": "Ludee", "email": " ",
+		"date": "22.11.2016", "comment": "updated header and license"},
+		{"name": "Ludee", "email": " ",
+		"date": "16.03.2016", "comment": "add license to source"} ],
 	"resources": [{
 		"schema": {
 			"fields": [
 				{"name": "id", "description": "unique identifier", "unit": "" },
 				{"name": "year", "description": "reference year", "unit": "" } ]},
-		"meta_version": "1.0"}] }';
+		"meta_version": "1.1" }] }';
 
 -- select description
 SELECT obj_description('schema.table' ::regclass) ::json;
--- documentation/script_header_example.sql
+++ documentation/script_header_example.sql
@@ -3,6 +3,9 @@ A description of the module (short but could be more than one line).
 Modules names should have short, all-lowercase names. 
 The module name may have underscores if this improves readability.
 
-__copyright__ = "Copyright ego developer group"
-__license__ = "GPLv3"
+__copyright__ 	= "Copyright Reiner Lemoine Institut gGmbH"
+__license__ 	= "GNU Affero General Public License Version 3 (AGPL-3.0)"
+__url__ 	= "https://github.com/openego/data_processing/blob/master/LICENSE"
+__author__ 	= "Ludee"
+__contains__	= " "
 */
