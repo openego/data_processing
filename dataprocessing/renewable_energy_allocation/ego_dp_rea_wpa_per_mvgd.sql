4b640e6aa7d72dc7eb2752cefb366c0c877bd9b7
Ludwig Huelk <ludwig.huelk@rl-institut.de>
2017-03-28 17:28:34 +0200

    update metadata version #23

--- dataprocessing/renewable_energy_allocation/ego_dp_rea_wpa_per_mvgd.sql
+++ dataprocessing/renewable_energy_allocation/ego_dp_rea_wpa_per_mvgd.sql
@@ -9,6 +9,8 @@ __url__ 	= "https://github.com/openego/data_processing/blob/master/LICENSE"
 __author__ 	= "Ludee"
 */
 
+
+/* 
 -- wind potential area
 DROP TABLE IF EXISTS  	model_draft.ego_supply_wpa CASCADE;
 CREATE TABLE         	model_draft.ego_supply_wpa (
@@ -36,7 +38,7 @@ ALTER TABLE	model_draft.ego_supply_wpa OWNER TO oeuser;
 
 -- ego scenario log (version,io,schema_name,table_name,script_name,comment)
 SELECT ego_scenario_log('v0.2.6','temp','model_draft','ego_supply_wpa','ego_dp_rea_wpa_per_mvgd.sql',' ');
-
+ */
 /* -- validate (geom)
 DROP VIEW IF EXISTS	model_draft.ego_supply_wpa_error_geom_view CASCADE;
 CREATE VIEW		model_draft.ego_supply_wpa_error_geom_view AS 
@@ -59,36 +61,44 @@ SELECT f_drop_view('{ego_supply_wpa_error_geom_view}', 'calc_ego_re'); */
 -- wpa per mv-griddistrict
 DROP TABLE IF EXISTS  	model_draft.ego_supply_wpa_per_mvgd CASCADE;
 CREATE TABLE         	model_draft.ego_supply_wpa_per_mvgd (
-		id 		SERIAL NOT NULL,
+		id 		serial,
 		subst_id 	integer,
-		area_ha 	decimal,
+		area_ha 	double precision,
 		geom 		geometry(Polygon,3035),
 CONSTRAINT 	ego_supply_wpa_per_mvgd_pkey PRIMARY KEY (id));
 
 -- ego scenario log (version,io,schema_name,table_name,script_name,comment)
 SELECT ego_scenario_log('v0.2.6','input','model_draft','ego_grid_mv_griddistrict','ego_dp_rea_wpa_per_mvgd.sql',' ');
 
+-- ego scenario log (version,io,schema_name,table_name,script_name,comment)
+SELECT ego_scenario_log('v0.2.6','input','supply','soethe_wind_potential_area','ego_dp_rea_wpa_per_mvgd.sql',' ');
+
 -- insert wpa per mv-griddistrict
+WITH	wpa_dump AS (
+	SELECT	(ST_DUMP(ST_MULTI(ST_UNION(
+			ST_BUFFER(ST_BUFFER(ST_TRANSFORM(geom,3035),-0,01),0,01)
+		)))).geom AS geom
+	FROM	supply.soethe_wind_potential_area)
 INSERT INTO     model_draft.ego_supply_wpa_per_mvgd (area_ha, geom)
-	SELECT	ST_AREA(cut.geom)/10000,
-		cut.geom ::geometry(Polygon,3035)
-	FROM	(SELECT ST_MakeValid((ST_DUMP(ST_MULTI(ST_SAFE_INTERSECTION(pot.geom,dis.geom)))).geom) AS geom
-		FROM	model_draft.ego_supply_wpa AS pot,
-			model_draft.ego_grid_mv_griddistrict AS dis
-		WHERE	pot.geom && dis.geom
-		) AS cut
-	WHERE	ST_IsValid(cut.geom) = 't' AND ST_GeometryType(cut.geom) = 'ST_Polygon' ;
+	SELECT	ST_AREA(c.geom)/10000,
+		c.geom ::geometry(Polygon,3035)
+	FROM	(SELECT ST_MakeValid((ST_DUMP(ST_MULTI(ST_SAFE_INTERSECTION(a.geom,b.geom)))).geom) AS geom
+		FROM	wpa_dump AS a,
+			model_draft.ego_grid_mv_griddistrict AS b
+		WHERE	a.geom && b.geom
+		) AS c
+	WHERE	ST_IsValid(c.geom) = 't' AND ST_GeometryType(c.geom) = 'ST_Polygon' ;
 
 -- substation id
 UPDATE 	model_draft.ego_supply_wpa_per_mvgd AS t1
 	SET  	subst_id = t2.subst_id
 	FROM    (
-		SELECT	pot.id AS id,
-			gd.subst_id AS subst_id
-		FROM	model_draft.ego_supply_wpa_per_mvgd AS pot,
-			model_draft.ego_grid_mv_griddistrict AS gd
-		WHERE  	gd.geom && pot.geom AND
-			ST_CONTAINS(gd.geom,ST_PointOnSurface(pot.geom))
+		SELECT	a.id AS id,
+			b.subst_id AS subst_id
+		FROM	model_draft.ego_supply_wpa_per_mvgd AS a,
+			model_draft.ego_grid_mv_griddistrict AS b
+		WHERE  	b.geom && a.geom AND
+			ST_CONTAINS(b.geom,ST_PointOnSurface(a.geom))
 		) AS t2
 	WHERE  	t1.id = t2.id;
 
@@ -101,22 +111,25 @@ ALTER TABLE	model_draft.ego_supply_wpa_per_mvgd OWNER TO oeuser;
 
 -- metadata
 COMMENT ON TABLE model_draft.ego_supply_wpa_per_mvgd IS '{
-	"title": "eGoDP_REA - wpa per mv-griddistrict",
-	"description": "potential areas for wind power plants - Wind Potential Area (wpa)",
+	"title": "eGo dataprocessing - WPA per MV Griddistrict",
+	"description": "Potential areas for wind power plants - Wind Potential Area (wpa)",
 	"language": [ "eng" ],
 	"reference_date": "",
 	"sources": [
-		{"name": "supply.soethe_wind_potential_area","description": "","url": ""},
-		{"name": "model_draft.ego_grid_mv_griddistrict","description": "","url": ""} ],
+		{"name": "eGo dataprocessing", "description": "v0.2.6",
+		"url": "https://github.com/openego/data_processing", "license": "GNU Affero General Public License Version 3 (AGPL-3.0)"},
+		{"name": " ", "description": " ",
+		"url": " ", "license": " "} ],
+		{"name": "supply.soethe_wind_potential_area","description": "","url": ""} ],
 	"spatial": [
 		{"extend": "Germany",
 		"resolution": "50m"} ],
 	"license": [
-		{"id": "tba",
-		"name": "tba",
-		"version": "tba",
-		"url": "tba",
-		"instruction": "tba"} ],
+		{"id": "ODbL-1.0",
+		"name": "Open Data Commons Open Database License 1.0",
+		"version": "1.0",
+		"url": "https://opendatacommons.org/licenses/odbl/1.0/",
+		"instruction": "You are free: To Share, To Create, To Adapt; As long as you: Attribute, Share-Alike, Keep open!"} ],
 	"contributors": [
 		{"name": "Ludwig Hülk",	"email": "ludwig.huelk@rl-institut.de",
 		"date": "01.10.2016", "comment": "Create table"},
