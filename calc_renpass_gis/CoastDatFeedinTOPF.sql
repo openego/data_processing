de3d4baf002752e36f06d886be40c6a0bce97616
ulfmueller <ulf.p.mueller@fh-flensburg.de>
2017-04-24 14:59:55 +0200

    Update CoastDatFeedinTOPF.sql

--- calc_renpass_gis/CoastDatFeedinTOPF.sql
+++ calc_renpass_gis/CoastDatFeedinTOPF.sql
@@ -4,7 +4,7 @@
 __copyright__ = "ZNES"
 __license__ = "GNU Affero General Public License Version 3 (AGPL-3.0)"
 __url__ = "https://github.com/openego/data_processing/blob/master/LICENSE"
-__author__ = "s3pp"
+__author__ = "wolfbunke"
 
 */
 
