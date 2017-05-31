f7494f93df66656c68b60a35094d70cb0de78b42
Lukas <lukas.wienholt@next-energy.de>
2017-05-31 12:04:06 +0200

    updated parameters for extendable storages

--- dataprocessing/sql_snippets/ego_dp_powerflow_lopf_data.sql
+++ dataprocessing/sql_snippets/ego_dp_powerflow_lopf_data.sql
@@ -72,7 +72,7 @@ FROM (
 	GROUP BY T2.generator_id
 ) T3 WHERE T3.generator_id = Y.generator_id;
 
-/*
+
 -- set storage parameters to every node with p_nom_extendable as true
 -- by Lukas
 
@@ -100,29 +100,81 @@ INSERT INTO model_draft.ego_grid_pf_hv_storage
   efficiency_dispatch, 
   standing_loss 
 )
-SELECT 'Status Quo',
+SELECT 
+  'Status Quo',
   nextval('orig_geo_powerplants.pf_storage_single_aggr_id') as aggr_id,
-  model_draft.ego_grid_hvmv_substation.otg_id,  -- assign one storage to each substation
+  model_draft.ego_grid_pf_hv_bus.bus_id,  -- assign one storage to each substation
   'flexible', 	-- set storage to be flexible
   'PQ',  -- PQ control
   0,  -- initial power of storage is 0
-  true, -- storage is extendable
-  0, -- there is no lower limit to storage power
+  false, -- storage is set to extendable in python script
+  0, -- there is no lower limit to storage power 
   1000000, -- inf could not be defined, thus very high number
-  0, -- default
+  -1, -- default
   1, -- default
   1, -- default
-  null, -- source could not be set to 0, so null.
-  (39.5156 + 1.5 + 1), -- marginal costs are those of oil / source no.4 plus 1
-  1000, -- this is an assumption as of now, must be defined when calculating for more than 1 or 2 hours
-  1, -- efficiency will be set via efficiency_store and efficiency_dispatch
+  11, -- source is for now the one of pumped_storage
+  0.01, -- marginal costs are set low in order to dispatch after RES and before conv. PP
+  229357, -- set according to Acatech2015 for Lithium-Ion. Annual cost method based on investment costs per MW. Must be reduced to size of snapshots if extendable storage shall be used.
+  0.855, -- efficiency according to Acatech2015
   0, -- initial storage level is 0
   false, -- cyclic state of charge is false
-  10, -- max hours is set to 10, just an assumption as of now
+  6, -- set to 6, to represent daily storage operation  
   1, -- no losses for storing
   1, -- no losses for dispatch
   0 -- no standing losses
-FROM model_draft.ego_grid_hvmv_substation;
-*/
+FROM model_draft.ego_grid_pf_hv_bus
+WHERE scn_name = 'Status Quo';
+
+INSERT INTO model_draft.ego_grid_pf_hv_storage
+(scn_name,
+  storage_id,
+  bus,
+  dispatch, 
+  control, 
+  p_nom, 
+  p_nom_extendable,
+  p_nom_min,
+  p_nom_max, 
+  p_min_pu_fixed, 
+  p_max_pu_fixed,
+  sign, 
+  source,
+  marginal_cost, 
+  capital_cost, 
+  efficiency,
+  soc_initial, 
+  soc_cyclic, 
+  max_hours,
+  efficiency_store, 
+  efficiency_dispatch, 
+  standing_loss 
+)
+SELECT 
+  'NEP 2035',
+  nextval('orig_geo_powerplants.pf_storage_single_aggr_id') as aggr_id,
+  model_draft.ego_grid_pf_hv_bus.bus_id,  -- assign one storage to each substation
+  'flexible', 	-- set storage to be flexible
+  'PQ',  -- PQ control
+  0,  -- initial power of storage is 0
+  false, -- storage is set to extendable in python script
+  0, -- there is no lower limit to storage power 
+  1000000, -- inf could not be defined, thus very high number
+  -1, -- default
+  1, -- default
+  1, -- default
+  11, -- source is for now the one of pumped_storage
+  0.01, -- marginal costs are set low in order to dispatch after RES and before conv. PP
+  65822, -- set according to Acatech2015 for Lithium-Ion (base year is 2023, not 2035!). Annual cost method based on investment costs per MW. Must be reduced to size of snapshots if extendable storage shall be used.
+  0.87, -- efficiency according to Acatech2015
+  0, -- initial storage level is 0
+  false, -- cyclic state of charge is false
+  6, -- set to 6, to represent daily storage operation  
+  1, -- no losses for storing
+  1, -- no losses for dispatch
+  0 -- no standing losses
+FROM model_draft.ego_grid_pf_hv_bus
+WHERE scn_name = 'NEP 2035';
+
 -- ego scenario log (version,io,schema_name,table_name,script_name,comment)
 SELECT ego_scenario_log('v0.2.10','output','model_draft','ego_grid_pf_hv_generator_pq_set','ego_dp_powerflow_lopf_data.sql',' ');
