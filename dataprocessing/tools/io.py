50d24c0982e023940c0c6b0aff65d539cbdeb8de
Ludwig Huelk <ludwig.huelk@rl-institut.de>
2017-06-22 15:42:04 +0200

    change tab for testing

--- dataprocessing/tools/io.py
+++ dataprocessing/tools/io.py
@@ -4,28 +4,28 @@ import getpass
 import sys
 
 def oedb_session(section='oep'):
-    """Get SQLAlchemy session object with valid connection to OEDB"""
+	"""Get SQLAlchemy session object with valid connection to OEDB"""
 
-    # get session object by oemof.db tools (requires ~/.egoio/config.ini)
-    try:
-        conn = connection(section=section)
+	# get session object by oemof.db tools (requires ~/.egoio/config.ini)
+	try:
+		conn = connection(section=section)
 
-    except:
-        print('Please provide connection parameters to database:\n' +
-              'Hit [Enter] to take defaults')
+	except:
+		print('Please provide connection parameters to database:\n' +
+			'Hit [Enter] to take defaults')
 
-        host = input('host (default 141.44.24.88): ') or 'oe.iws.cs.ovgu.de'
-        port = input('port (default 5432): ') or '5432'
-        database = input("database name (default 'oep'): ") or 'oep'
-        user = input('user (default postgres): ')
-        # password = input('password: ')
-        password = getpass.getpass(prompt='password: ',
-                                   stream=sys.stderr)
-        conn = create_engine(
-            'postgresql://' + '%s:%s@%s:%s/%s' % (user,
-                                                  password,
-                                                  host,
-                                                  port,
-                                                  database)).connect()
+		host = input('host (default 141.44.24.88): ') or 'oe.iws.cs.ovgu.de'
+		port = input('port (default 5432): ') or '5432'
+		database = input("database name (default 'oep'): ") or 'oep'
+		user = input('user (default postgres): ')
+		# password = input('password: ')
+		password = getpass.getpass(prompt='password: ',
+								   stream=sys.stderr)
+		conn = create_engine(
+			'postgresql://' + '%s:%s@%s:%s/%s' % (user,
+												  password,
+												  host,
+												  port,
+												  database)).connect()
 
-    return conn
+	return conn
