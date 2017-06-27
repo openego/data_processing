a609c92094724d035cf9582cd2ad81d93c2ffba8
Ludwig Huelk <ludwig.huelk@rl-institut.de>
2017-06-27 14:10:22 +0200

    add url to vacuum script #138

--- dataprocessing/sql_snippets/ego_dp_vacuum_full.sql
+++ dataprocessing/sql_snippets/ego_dp_vacuum_full.sql
@@ -1,11 +1,11 @@
 /*
-Check the necessary input tables.
-Return version of input tables.
+Do a VACUUM FULL ANALYZE
 
 __copyright__ 	= "Reiner Lemoine Institut"
 __license__ 	= "GNU Affero General Public License Version 3 (AGPL-3.0)"
 __url__ 	= "https://github.com/openego/data_processing/blob/master/LICENSE"
 __author__ 	= "Ludee"
+__contains__	= "https://www.postgresql.org/docs/current/static/sql-vacuum.html"
 */
 
 VACUUM FULL ANALYZE oedb;
