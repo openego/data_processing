dbed15b899eb383843e15040a79b8c2c4d69bdcd
Ludwig Huelk <ludwig.huelk@rl-institut.de>
2017-06-27 14:07:08 +0200

    add vacuum script #138

---
+++ dataprocessing/sql_snippets/ego_dp_vacuum_full.sql
@@ -0,0 +1,11 @@
+/*
+Check the necessary input tables.
+Return version of input tables.
+
+__copyright__ 	= "Reiner Lemoine Institut"
+__license__ 	= "GNU Affero General Public License Version 3 (AGPL-3.0)"
+__url__ 	= "https://github.com/openego/data_processing/blob/master/LICENSE"
+__author__ 	= "Ludee"
+*/
+
+VACUUM FULL ANALYZE oedb;
