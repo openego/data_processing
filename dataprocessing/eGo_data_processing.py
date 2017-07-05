de99d80b316824a385a7f25d002f171306378058
Ilka <ilka.cussmann@fh-flensburg.de>
2017-07-05 10:51:19 +0200

    adjust dp to eGo 100 scenario

--- dataprocessing/eGo_data_processing.py
+++ dataprocessing/eGo_data_processing.py
@@ -108,14 +108,12 @@ def data_processing():
 ##
 #	'ego_dp_powerflow_grid_future_scenarios.sql',		# Copy grid to future scenarios
 #  	'ego_dp_powerflow_assignment_generator.sql',  		# Assign generators to corresponding substation (SQ, NEP2035, eGo100)
-#	'ego_dp_powerflow_assignment_load.sql',        		# Assign loads to their corresponding substation
+#	'ego_dp_powerflow_assignment_load.sql',        		# Assign loads to their corresponding substation (SQ, NEP2035, eGo100)
 
-#	'ego_dp_powerflow_assignment_load_nep2035.sql',		# Copy loads from SQ scenario
-#	'ego_dp_powerflow_timeseries_generator.sql',			# Transfer renpassG!S results into the corresponding powerflow table
-#	'ego_dp_powerflow_griddistrict_demand.py',			# Demand per MV Griddistrict
-#	'ego_dp_powerflow_timeseries_demand.sql',				# Insert demand series into corresponding powerflow table
-#	'ego_dp_powerflow_lopf_data.sql',						# Set marginal costs for generators and storages
-#	'ego_dp_powerflow_load_timeseries_NEP2035.sql', 		# Copy demand timeseries to NEP 2035 scenario
+#	'ego_dp_powerflow_timeseries_generator.sql',		# Transfer renpassG!S results into the corresponding powerflow table
+#	'ego_dp_powerflow_griddistrict_demand.py',		# Demand per MV Griddistrict
+#	'ego_dp_powerflow_timeseries_demand.sql',		# Insert demand series into corresponding powerflow table (SQ, NEP2035, eGo100)
+#	'ego_dp_powerflow_lopf_data.sql',			# Set marginal costs for generators and storages
 #	
 #	
     ## VERSIONING
--- dataprocessing/sql_snippets/ego_dp_powerflow_timeseries_demand.sql
+++ dataprocessing/sql_snippets/ego_dp_powerflow_timeseries_demand.sql
@@ -50,3 +50,31 @@ INSERT INTO model_draft.ego_grid_pf_hv_load_pq_set (scn_name, load_id, temp_id, 
 
 -- ego scenario log (version,io,schema_name,table_name,script_name,comment)
 SELECT ego_scenario_log('v0.2.10','output','model_draft','ego_grid_pf_hv_load_pq_set','ego_dp_powerflow_timeseries_demand.sql',' ');
+
+
+---------------
+-- Add load time series to future scenarios equivalent to Status Quo scenario
+---------------
+
+
+-- Include data from .load_pq_set for scenario 'NEP 2035' equivalent to Status Quo 
+
+DELETE FROM model_draft.ego_grid_pf_hv_load_pq_set WHERE scn_name = 'NEP 2035'; 
+
+INSERT INTO model_draft.ego_grid_pf_hv_load_pq_set
+SELECT 'NEP 2035', a.load_id, a.temp_id, a.p_set, a.q_set
+FROM model_draft.ego_grid_pf_hv_load_pq_set a
+WHERE scn_name= 'Status Quo';
+ 
+
+-- Include data from .load_pq_set for scenario 'eGo 100' equivalent to Status Quo 
+
+DELETE FROM model_draft.ego_grid_pf_hv_load_pq_set WHERE scn_name = 'eGo 100'; 
+
+INSERT INTO model_draft.ego_grid_pf_hv_load_pq_set
+SELECT 'eGo 100', a.load_id, a.temp_id, a.p_set, a.q_set
+FROM model_draft.ego_grid_pf_hv_load_pq_set a
+WHERE scn_name= 'Status Quo';
+
+-- ego scenario log (version,io,schema_name,table_name,script_name,comment)
+SELECT ego_scenario_log('v0.2.10','output','model_draft','ego_grid_pf_hv_load_pq_set','ego_dp_powerflow_load_timeseries_NEP2035.sql',' ');
