38fea1b1bcad608488622dfa3a55c976b4e0ab23
Ludwig Hülk <ludwig.huelk@rl-institut.de>
2017-07-06 12:14:00 +0200

    add rename script #142

---
+++ preprocessing/sql_snippets/oedb_setup_schema_rename.sql
@@ -0,0 +1,9 @@
+/*
+Setup the OpenEnergy Database (oedb) schema structure
+
+__copyright__ 	= "Reiner Lemoine Institut"
+__license__ 	= "GNU Affero General Public License Version 3 (AGPL-3.0)"
+__url__ 	= "https://github.com/openego/data_processing/blob/master/LICENSE"
+__author__ 	= "Ludee"
+*/
+
