53e3a3962b4c590f6d8eb0f27c2572a515de05ca
wolfbunke <wolf-dieter.bunke@uni-flensburg.de>
2017-08-01 15:56:19 +0200

    add changes

--- preprocessing/sql_snippets/ego_dp_supply_add_coastdat_gid.sql
+++ preprocessing/sql_snippets/ego_dp_supply_add_coastdat_gid.sql
@@ -5,10 +5,6 @@ __copyright__ = "Europa-Universität Flensburg - ZNES"
 __license__ = "GNU Affero General Public License Version 3 (AGPL-3.0)"
 __url__ = "https://github.com/openego/data_processing/blob/master/LICENSE"
 __author__ = "wolfbunke"	
-  
-
-Todo: Add id
-      change metadata
       
 
 */
