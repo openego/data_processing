e818e5b921dc1429d634623b7c2ca13cb1831768
wolfbunke <wolf-dieter.bunke@uni-flensburg.de>
2017-08-28 11:02:38 +0200

    add file name

--- preprocessing/python_scripts/renpass_gis/simple_feedin/results_to_oedb.py
+++ preprocessing/python_scripts/renpass_gis/simple_feedin/results_to_oedb.py
@@ -26,7 +26,7 @@ import os
 
 # Settings
 #filename = '2017-08-07_simple_feedin_All_subids_weatherids_ego_weatherYear2011.csv'
-filename = 'simple_feedin_test.csv'
+filename = '2017-08-21_simple_feedin_ego-100-wj2011_all.csv'
 conn = conn
 # read configuration file
 path = os.path.join(os.path.expanduser("~"), '.open_eGo', 'config.ini')
@@ -36,17 +36,24 @@ config = readcfg(path=path)
 section = 'oedb'
 conn = dbconnect(section=section, cfg=config)
 
+print('Read CSV...')
 # load data from csv
-time_series = pd.read_csv(filename,skiprows=1)
-scenario_name = pd.read_csv(filename,header=None,nrows=1).iloc[0,0]
+time_series = pd.read_csv(filename,header=1)
 
-
-# prepare DataFrame
+# prepare DataFrames
+print('prepare DataFrames CSV...')
 columns = ['hour','coastdat_id','sub_id', 'generation_type', 'feedin', 'scenario']
 index = np.arange(1,len(time_series)-1)
 db_structure = pd.DataFrame(columns=columns, index=index)
+scenario_names = pd.read_csv(filename,header=None, nrows=2)
+scenario_names.columns =time_series.columns
 
-time_series.iloc[2,2]
+print('Start iteration...')
+
+# empty DB Table
+#db_empty = pd.DataFrame(columns=columns)
+#db_empty.to_sql('ego_simple_feedin_full', conn, schema='model_draft',
+# if_exists='replace',index=False)
 
 # Insert Data to DB by DataFrame
 for k in time_series.columns:
@@ -58,20 +65,21 @@ for k in time_series.columns:
     db_structure['coastdat_id'] = l
     db_structure['sub_id'] = p
     db_structure['hour'] = index
-    db_structure['generation_type'] = time_series.iloc[0,0]
-    db_structure['feedin'] = time_series[k].shift(-1)
-    db_structure['scenario'] = scenario_name
+    idx = pd.IndexSlice
+    db_structure['generation_type'] = time_series[k].values[0]
+    db_structure['feedin'] = time_series[k].shift(-2)
+    db_structure['scenario'] = scenario_names[k].values[0]
     db_structure = db_structure.reset_index(drop=True)
 
     # write to csv
     if not os.path.isfile('result_feedin.csv'):
-        db_structure.to_csv('result_feedin.csv',header ='columns')
+        db_structure.to_csv('result_feedin.csv',header ='columns',index = False)
     else: # else it exists so append without writing the header
-        db_structure.to_csv('result_feedin.csv',mode = 'a',header=False)
+        db_structure.to_csv('result_feedin.csv',mode = 'a',header=False,index = False)
 
     # write df to database
     #db_structure.to_sql('ego_simple_feedin_full', conn, schema='model_draft',
-    #if_exists='append',index=False)
+     #if_exists='append',index=False)
 
 print('Done!')
 
