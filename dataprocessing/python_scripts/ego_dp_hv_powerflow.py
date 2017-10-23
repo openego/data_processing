adc4a8a38da1ca0943800730d30482a860cf34d3
Martin <martin.soethe@uni-flensburg.de>
2017-10-23 22:00:58 +0200

    Remove load function in hv setup script

--- dataprocessing/python_scripts/ego_dp_hv_powerflow.py
+++ dataprocessing/python_scripts/ego_dp_hv_powerflow.py
@@ -1,42 +1,39 @@
+""" This script creates the initial HV powerflow
+"""
+
+__copyright__ = "ZNES Flensburg"
+__license__ = "GNU Affero General Public License Version 3 (AGPL-3.0)"
+__url__ = "https://github.com/openego/data_processing/blob/master/LICENSE"
+__author__ = ""
+
+
+import os, json, csv
 from oemof.db import connection
-from egoio.db_tables.model_draft import Base
+from oemof.db.tools import change_owner_to, grant_db_access
+from dataprocessing.tools.io import load_external_file
+from egoio.db_tables.model_draft import Base, EgoGridPfHvSource
 from sqlalchemy.orm import sessionmaker
-import collections
-import os
-import json
-from json import JSONDecodeError
-
-conn = connection(section='local')
-session = sessionmaker(bind=conn)()
-engine = conn.engine
-# use context manager for path manipulation
-# https://stackoverflow.com/questions/431684/how-do-i-cd-in-python/13197763#13197763
-cwd = os.getcwd()
-dirname = "/home/martin/open_eGo/data_processing/dataprocessing/python_scripts"
-#wdir = os.path.dirname(__file__)
-os.chdir(dirname)
-
-
-def loadcomment(fpath):
-    """ Load comment JSON-file
-
-    Raises
-    ------
-    FileNotFoundError
-    """
-    try:
-        with open(fpath) as f:
-            return json.load(f, object_pairs_hook=collections.OrderedDict)
-    except FileNotFoundError:
-        print('No comment in JSON-format found at %s.' % fpath)
-
-# https://stackoverflow.com/questions/38678336/sqlalchemy-how-to-implement-drop-table-cascade
 from sqlalchemy.schema import DropTable
 from sqlalchemy.ext.compiler import compiles
+
+# Adapt SQL-Alchemy's postgresql dialect to include the CASCADE statement on
+# DROP TABLE as described here:
+# https://stackoverflow.com/questions/38678336/sqlalchemy-how-to-implement-drop-table-cascade
 @compiles(DropTable, "postgresql")
 def _compile_drop_table(element, compiler, **kwargs):
     return compiler.visit_drop_table(element) + " CASCADE"
 
+# TODO Maybe use context manager for path manipulation
+# https://stackoverflow.com/questions/431684/how-do-i-cd-in-python/13197763#13197763
+conn = connection(section='local')
+session = sessionmaker(bind=conn)()
+engine = conn.engine
+cwd = os.getcwd()
+dirname = "/home/martin/open_eGo/data_processing/dataprocessing/python_scripts"
+# wdir = os.path.dirname(__file__)
+os.chdir(dirname)
+
+###############################################################################
 tablenames = ['model_draft.ego_grid_pf_hv_scenario_settings',
               'model_draft.ego_grid_pf_hv_source',
               'model_draft.ego_grid_pf_hv_bus',
