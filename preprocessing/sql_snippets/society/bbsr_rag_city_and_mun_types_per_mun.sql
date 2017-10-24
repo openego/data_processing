39be8d86ae6fd930b5eed4a6480edb1610baa119
KilianZimmerer <kikleer@altlautern.de>
2017-10-24 10:54:18 +0200

    added script-header

--- preprocessing/sql_snippets/society/bbsr_rag_city_and_mun_types_per_mun.sql
+++ preprocessing/sql_snippets/society/bbsr_rag_city_and_mun_types_per_mun.sql
@@ -1,3 +1,15 @@
+/*
+A description of the module (short but could be more than one line).
+Modules names should have short, all-lowercase names. 
+The module name may have underscores if this improves readability.
+
+__copyright__ 	= "Copyright Reiner Lemoine Institut gGmbH"
+__license__ 	= "GNU Affero General Public License Version 3 (AGPL-3.0)"
+__url__ 	= "https://github.com/openego/data_processing/blob/master/LICENSE"
+__author__ 	= "KilianZimmerer"
+__contains__	= "url"
+*/
+
 -- metadata
 COMMENT ON TABLE society.bbsr_rag_city_and_mun_types_per_mun IS '{
     "title": "BBSR - Raumabgrenzungen - Stadt- und Gemeindetyp - 2013",
