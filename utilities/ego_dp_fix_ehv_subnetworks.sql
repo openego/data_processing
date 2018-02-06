09f2531fb9d56baa7e8de8a37c2319ed376fe0e6
ClaraBuettner <clarabuettner@aol.com>
2018-02-06 16:44:07 +0100

    Update to implement in dp.

--- utilities/ego_dp_fix_ehv_subnetworks.sql
+++ utilities/ego_dp_fix_ehv_subnetworks.sql
@@ -1,8 +1,21 @@
-﻿---- 380kV Berlin 
+﻿DROP SEQUENCE IF EXISTS model_draft.ego_grid_hv_fix_errors_bus_id CASCADE;
+CREATE SEQUENCE model_draft.ego_grid_hv_fix_errors_bus_id;
+SELECT setval('model_draft.ego_grid_hv_fix_errors_bus_id', (max(bus_id)+1)) FROM model_draft.ego_grid_pf_hv_bus;
+
+DROP SEQUENCE IF EXISTS  model_draft.ego_grid_hv_fix_errors_line_id CASCADE;
+CREATE SEQUENCE model_draft.ego_grid_hv_fix_errors_line_id;
+SELECT setval('model_draft.ego_grid_hv_fix_errors_line_id', (max(line_id)+1)) FROM model_draft.ego_grid_pf_hv_line;
+
+DROP SEQUENCE IF EXISTS model_draft.ego_grid_hv_fix_errors_transformer_id CASCADE;
+CREATE SEQUENCE model_draft.ego_grid_hv_fix_errors_transformer_id;
+SELECT setval('model_draft.ego_grid_hv_fix_errors_transformer_id', (max(trafo_id)+1)) FROM model_draft.ego_grid_pf_hv_transformer;
+
+
+---- 380kV Berlin 
 --- line doesn't exist in entso-e, substations are 110kV in osm 
 -- delete 380kV buses nd lines and temporary reconnect one-ports
-DELETE FROM model_draft.ego_grid_pf_hv_busmap WHERE scn_name = 'Status Quo';
 
+/*
 UPDATE model_draft.ego_grid_pf_hv_generator
 SET	bus = (SELECT bus_i FROM grid.otg_ehvhv_bus_data WHERE osm_name = 'Umspannwerk Biesdorf Nord' AND base_kv = 110)
 WHERE bus = (SELECT bus_i FROM grid.otg_ehvhv_bus_data WHERE osm_name = 'Umspannwerk Biesdorf Nord' AND base_kv = 380);
@@ -21,7 +34,7 @@ WHERE bus = (SELECT bus_i FROM grid.otg_ehvhv_bus_data WHERE osm_name = 'Umspann
 
 UPDATE model_draft.ego_grid_pf_hv_storage
 SET	bus = (SELECT bus_i FROM grid.otg_ehvhv_bus_data WHERE osm_name = 'Umspannwerk Lichtenberg' AND base_kv = 110)
-WHERE bus = (SELECT bus_i FROM grid.otg_ehvhv_bus_data WHERE osm_name = 'Umspannwerk Lichtenberg' AND base_kv = 380);
+WHERE bus = (SELECT bus_i FROM grid.otg_ehvhv_bus_data WHERE osm_name = 'Umspannwerk Lichtenberg' AND base_kv = 380);*/
 
 DELETE FROM model_draft.ego_grid_pf_hv_bus WHERE v_nom = 380 AND bus_id IN (SELECT bus_i FROM grid.otg_ehvhv_bus_data
 WHERE geom IN ('0101000020E6100000E6D4C59C60F02A40A254AE08CF424A40','0101000020E61000000F6844C4280B2B4074AC9C07C1424A40', '0101000020E61000008A5DDBDB2DE92A40372C5789C3424A40', '0101000020E6100000799F91F85E0B2B40277E8D4F25434A40', '0101000020E6100000ADA580B4FF112B40829EBC7E77434A40', '0101000020E6100000C8C54A64F5122B4012EB782DCC444A40')
@@ -38,7 +51,7 @@ AND base_kv = 380);
 --- missing line from Weiher to Uchtelfangen
 -- add line
 INSERT INTO model_draft.ego_grid_pf_hv_line (line_id, bus0, bus1, s_nom, cables, frequency, topo, geom, length, x)
-SELECT	nextval('model_draft.ego_grid_hv_electrical_neighbours_line_id'),
+SELECT	nextval('model_draft.ego_grid_hv_fix_errorsline_id'),
 	(SELECT bus_i FROM grid.otg_ehvhv_bus_data WHERE base_kv = 220 AND osm_name = 'KW Weiher'),
 	(SELECT bus_i FROM grid.otg_ehvhv_bus_data WHERE base_kv = 220 AND osm_name = 'Umspannwerk Uchtelfangen'),
 	1040,
@@ -60,7 +73,7 @@ FROM model_draft.ego_grid_pf_hv_line WHERE topo = '0102000020E610000002000000D6D
 ---- 220kV Industriepark Höchst
 --- wrong voltage-level 
 -- change v_nom from 220kV to 110kV, add/delete buses, change one-ports buses
-UPDATE model_draft.ego_grid_pf_hv_generator
+/*UPDATE model_draft.ego_grid_pf_hv_generator
 SET	bus = (SELECT bus_i FROM grid.otg_ehvhv_bus_data WHERE geom = '0101000020E6100000C208F2600E1321404EED06B1FE094940' AND base_kv = 110)
 WHERE bus = (SELECT bus_i FROM grid.otg_ehvhv_bus_data WHERE geom = '0101000020E6100000C208F2600E1321404EED06B1FE094940' AND base_kv = 220);
 
@@ -83,10 +96,11 @@ WHERE bus = (SELECT bus_i FROM grid.otg_ehvhv_bus_data WHERE geom = '0101000020E
 UPDATE model_draft.ego_grid_pf_hv_storage
 SET	bus = (SELECT bus_i FROM grid.otg_ehvhv_bus_data WHERE geom = '0101000020E610000091281516C01521401894D558490C4940' AND base_kv = 110)
 WHERE bus = (SELECT bus_i FROM grid.otg_ehvhv_bus_data WHERE geom = '0101000020E610000091281516C01521401894D558490C4940' AND base_kv = 220);
+'*/
 
 INSERT INTO model_draft.ego_grid_pf_hv_bus (bus_id, v_nom, geom)
 SELECT DISTINCT ON (geom)
-	nextval('model_draft.ego_grid_hv_electrical_neighbours_bus_id'),
+	nextval('model_draft.ego_grid_hv_fix_errorsbus_id'),
 	110,
 	geom
 FROM grid.otg_ehvhv_bus_data WHERE geom = '0101000020E61000008FEB3AAF0C152140B97D456DD1094940';
@@ -126,7 +140,7 @@ DELETE FROM model_draft.ego_grid_pf_hv_bus WHERE bus_id IN
 --- add 220kV line from Uchtelfangen to Heinitz, in respect to osm and bing
 -- this error is maybe fixed in osm 
 INSERT INTO model_draft.ego_grid_pf_hv_line (line_id, bus0, bus1, length, topo, geom,  x, r, s_nom, cables, frequency)
-SELECT 	nextval('model_draft.ego_grid_hv_electrical_neighbours_line_id'),
+SELECT 	nextval('model_draft.ego_grid_hv_fix_errorsline_id'),
 	(SELECT bus_i FROM grid.otg_ehvhv_bus_data WHERE base_kv = 220 AND osm_name = 'Umspannwerk Uchtelfangen'),
 	(SELECT bus_i FROM grid.otg_ehvhv_bus_data WHERE base_kv = 220 AND osm_name = 'Umspannwerk Heinitz'),
 	10.6040557238013, 
@@ -151,7 +165,7 @@ WHERE scn_name = 'Status Quo' AND s_nom = 1040 AND topo = '0102000020E6100000020
 ---- Verbindung Lübeck-Siems
 INSERT INTO model_draft.ego_grid_pf_hv_bus (bus_id, v_nom, geom)
 SELECT DISTINCT ON (geom)
-	nextval('model_draft.ego_grid_hv_electrical_neighbours_bus_id'),
+	nextval('model_draft.ego_grid_hv_fix_errorsbus_id'),
 	220,
 	geom
 FROM grid.otg_ehvhv_bus_data WHERE osm_name IN ('Umspannwerk Siems');
@@ -167,7 +181,7 @@ FROM model_draft.ego_grid_pf_hv_bus
 WHERE scn_name = 'Status Quo' AND geom = '0101000020E6100000D38040358C8525403935408972F44A40' AND v_nom = 220;
 	
 INSERT INTO model_draft.ego_grid_pf_hv_line (line_id, bus0, bus1, s_nom, topo, geom, length, x, cables, frequency)
-SELECT	nextval('model_draft.ego_grid_hv_electrical_neighbours_line_id'),
+SELECT	nextval('model_draft.ego_grid_hv_fix_errorsline_id'),
 	(SELECT bus_i FROM grid.otg_ehvhv_bus_data WHERE base_kv = 220 AND osm_name = 'Lübeck'),
 	(SELECT DISTINCT ON (bus_id) bus_id FROM model_draft.ego_grid_pf_hv_bus WHERE v_nom = 220 AND geom = '0101000020E6100000D38040358C8525403935408972F44A40'),
 	520,
@@ -189,7 +203,7 @@ FROM model_draft.ego_grid_pf_hv_line
 WHERE scn_name = 'Status Quo' AND geom = '0105000020E6100000010000000102000000020000006380D3776E4825406764811FA7F54A40D38040358C8525403935408972F44A40' AND s_nom = 520;
 	
 INSERT INTO model_draft.ego_grid_pf_hv_transformer(trafo_id, bus0, bus1, s_nom, x, phase_shift, tap_ratio, topo, geom)
-SELECT 	nextval('model_draft.ego_grid_hv_electrical_neighbours_transformer_id'),
+SELECT 	nextval('model_draft.ego_grid_hv_fix_errorstransformer_id'),
 	(SELECT DISTINCT ON (bus_id) bus_id FROM model_draft.ego_grid_pf_hv_bus WHERE v_nom = 220 AND geom = '0101000020E6100000D38040358C8525403935408972F44A40'),
 	(SELECT DISTINCT ON (bus_id) bus_id FROM model_draft.ego_grid_pf_hv_bus WHERE v_nom = 380 AND geom = '0101000020E6100000D38040358C8525403935408972F44A40'),
 	600,
@@ -213,7 +227,7 @@ WHERE scn_name = 'Status Quo' AND geom = '0105000020E610000001000000010200000002
 --- wrong line connection 
 -- delete old line and add new one
 INSERT INTO model_draft.ego_grid_pf_hv_bus (bus_id, v_nom, geom)
-VALUES	(nextval('model_draft.ego_grid_hv_electrical_neighbours_bus_id'),
+VALUES	(nextval('model_draft.ego_grid_hv_fix_errorsbus_id'),
 	380,
 	'0101000020E6100000E1F4899702B52940FA1DD48F97614840');
 	
@@ -228,7 +242,7 @@ FROM model_draft.ego_grid_pf_hv_bus
 WHERE scn_name = 'Status Quo' AND geom = '0101000020E6100000E1F4899702B52940FA1DD48F97614840' AND v_nom = 380;
 
 INSERT INTO model_draft.ego_grid_pf_hv_line (line_id, bus0, bus1, topo, geom,length,  x,  s_nom, cables, frequency)
-VALUES (nextval('model_draft.ego_grid_hv_electrical_neighbours_line_id'),
+VALUES (nextval('model_draft.ego_grid_hv_fix_errorsline_id'),
 	(SELECT bus_i FROM grid.otg_ehvhv_bus_data WHERE base_kv = 380 AND osm_name = 'Umspannwerk Pleinting'),
 	(SELECT DISTINCT ON (bus_id) bus_id FROM  model_draft.ego_grid_pf_hv_bus WHERE v_nom = 380 AND geom = '0101000020E6100000E1F4899702B52940FA1DD48F97614840'),
 	'0102000020E61000000200000030AC43C5F4342A40D8CA9DE357554840E1F4899702B52940FA1DD48F97614840',
@@ -239,7 +253,7 @@ VALUES (nextval('model_draft.ego_grid_hv_electrical_neighbours_line_id'),
 	3,
 	50),
 
-	(nextval('model_draft.ego_grid_hv_electrical_neighbours_line_id'),
+	(nextval('model_draft.ego_grid_hv_fix_errorsline_id'),
 	(SELECT DISTINCT ON (bus_id) bus_id FROM  model_draft.ego_grid_pf_hv_bus WHERE v_nom = 380 AND geom = '0101000020E6100000E1F4899702B52940FA1DD48F97614840'),
 	(SELECT bus_i FROM grid.otg_ehvhv_bus_data WHERE base_kv = 380 AND osm_name = 'Schwandorf'),
 	'0102000020E610000002000000E1F4899702B52940FA1DD48F97614840BBEF0B431D2A2840230697C412A64840',
@@ -250,7 +264,7 @@ VALUES (nextval('model_draft.ego_grid_hv_electrical_neighbours_line_id'),
 	3,
 	50),
 
-	(nextval('model_draft.ego_grid_hv_electrical_neighbours_line_id'),
+	(nextval('model_draft.ego_grid_hv_fix_errorsline_id'),
 	(SELECT DISTINCT ON (bus_id) bus_id FROM  model_draft.ego_grid_pf_hv_bus WHERE v_nom = 380 AND geom = '0101000020E6100000E1F4899702B52940FA1DD48F97614840'),
 	(SELECT bus_i FROM grid.otg_ehvhv_bus_data WHERE base_kv = 380 AND geom = '0101000020E6100000C000DCEF17B429409E0779BEB3614840'),
 	'0102000020E610000002000000E1F4899702B52940FA1DD48F97614840C000DCEF17B429409E0779BEB3614840',
@@ -281,7 +295,7 @@ DELETE FROM model_draft.ego_grid_pf_hv_bus WHERE geom = '0101000020E61000002F70D
 --- missing connection from powerstation to 380kV line
 -- add 380kV line
 INSERT INTO model_draft.ego_grid_pf_hv_line (line_id, bus0, bus1, topo, geom,length,  x,  s_nom, cables, frequency)
-SELECT 	nextval('model_draft.ego_grid_hv_electrical_neighbours_line_id'),
+SELECT 	nextval('model_draft.ego_grid_hv_fix_errorsline_id'),
 	(SELECT bus_i FROM grid.otg_ehvhv_bus_data WHERE base_kv = 380 AND geom = '0101000020E610000050D9BE3268491940C27D4210236B4940'),
 	(SELECT bus_i FROM grid.otg_ehvhv_bus_data WHERE base_kv = 380 AND geom = '0101000020E6100000FE4C1828844919408E469968EB6A4940'),
 	'0102000020E61000000200000050D9BE3268491940C27D4210236B4940FE4C1828844919408E469968EB6A4940',
@@ -301,7 +315,5 @@ INSERT INTO model_draft.ego_grid_pf_hv_line (scn_name, line_id, bus0, bus1, topo
 SELECT 'eGo 100', line_id, bus0, bus1, topo, geom,length,  x,  s_nom, cables, frequency
 FROM model_draft.ego_grid_pf_hv_line
 WHERE scn_name = 'Status Quo' AND topo = '0102000020E61000000200000050D9BE3268491940C27D4210236B4940FE4C1828844919408E469968EB6A4940' AND s_nom = 3580;
-
-
 
 DELETE FROM model_draft.ego_grid_pf_hv_transformer WHERE bus1 NOT IN (SELECT bus_id FROM model_draft.ego_grid_pf_hv_bus) OR bus0 NOT IN (SELECT bus_id FROM model_draft.ego_grid_pf_hv_bus);
