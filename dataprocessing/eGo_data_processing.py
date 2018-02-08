de926cfb1689d3f0ce2737984f128e796973d4db
Ludee <Ludwig.Huelk@rl-institut.de>
2018-02-08 10:23:22 +0100

    add bpmn section gd

--- dataprocessing/eGo_data_processing.py
+++ dataprocessing/eGo_data_processing.py
@@ -4,7 +4,7 @@ Reads python and SQL scripts and gives logging infos during the execution.
 Also see corresponding BPML diagram.
 """
 
-__copyright__ = "Reiner Lemoine Institut gGmbH"
+__copyright__ = "Reiner Lemoine Institut"
 __license__ = "GNU Affero General Public License Version 3 (AGPL-3.0)"
 __url__ = "https://github.com/openego/data_processing/blob/master/LICENSE"
 __author__ = "gplssm, Ludee"
