d5cb1e3e1c0fd79b6437ded1305c0d6aa4e37a0b
Martin <martin.soethe@uni-flensburg.de>
2018-04-09 22:10:58 +0200

    Separate assignment of p_max_pu

---
+++ dataprocessing/python_scripts/ego_dp_powerflow_timeseries_feedin.py
@@ -0,0 +1,61 @@
+import pandas as pd
+
+from dataprocessing.tools.io import oedb_session
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import case
+from dataprocessing.python_scripts.functions.ego_scenario_log \
+    import write_ego_scenario_log
+
+from egoio.db_tables.model_draft import EgoSupplyPfGeneratorSingle \
+    as GeneratorSingle, EgoGridPfHvGeneratorPqSet as PqSet
+from ego_dp_powerflow_timeseries_generator_helper import FUEL_TO_SOURCE, SCENARIOMAP, \
+    TEMPID, missing_orm_classes
+
+# get database connection
+conn = oedb_session(section='test')
+Session = sessionmaker(bind=conn)
+session = Session()
+
+PowerClass, Feedin, *_, Results = missing_orm_classes(session)
+
+# p_max_pu
+for scn_name, scn_nr in SCENARIOMAP.items():
+
+    sources = ['wind_onshore', 'wind_offshore', 'solar']
+    sources_dict = {k: v for k, v in FUEL_TO_SOURCE.items() if k in sources}
+    casestr = case(sources_dict, value=Feedin.source, else_=None)
+
+    # construct subquery with unique aggr_id, source, w_id, power_class
+    filters = (GeneratorSingle.scn_name == scn_name, GeneratorSingle.aggr_id != None)
+    fields = [GeneratorSingle.aggr_id, GeneratorSingle.source,
+              GeneratorSingle.w_id, GeneratorSingle.power_class]
+    grouper = GeneratorSingle.aggr_id, GeneratorSingle.source,\
+        GeneratorSingle.w_id, GeneratorSingle.power_class
+
+    t = session.query(*fields).group_by(*grouper).filter(*filters).subquery()
+
+    # use subquery to select the corresponding feedin
+    query = session.query(t, Feedin.feedin.label('p_max_pu')).filter(t.c.w_id == Feedin.w_id,
+        t.c.power_class == Feedin.power_class, t.c.source == casestr)
+
+    generators = pd.read_sql(query.statement, query.session.bind)
+
+    # rename column aggr_id to generator_id
+    generators.rename(columns={'aggr_id': 'generator_id'}, inplace=True)
+
+    generators['temp_id'] = TEMPID
+    generators['scn_name'] = scn_name
+
+    fields = ['generator_id', 'p_max_pu', 'scn_name', 'temp_id']
+    for i in generators[fields].to_dict(orient='records'):
+        session.add(PqSet(**i))
+
+    session.commit()
+
+    write_ego_scenario_log(conn=conn,
+                           version='v0.3.0',
+                           io='input',
+                           schema='model_draft',
+                           table=PqSet.__tablename__,
+                           script=__file__,
+                           entries=len(generators))
