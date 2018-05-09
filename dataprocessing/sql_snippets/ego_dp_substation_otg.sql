6d861726a97cb646e88a05fced37396cfccbbe5e
IlkaCu <ilka.cussmann@hs-flensburg.de>
2018-05-09 16:41:02 +0200

    Update ego_dp_substation_otg.sql

--- dataprocessing/sql_snippets/ego_dp_substation_otg.sql
+++ dataprocessing/sql_snippets/ego_dp_substation_otg.sql
@@ -25,6 +25,8 @@ UPDATE model_draft.ego_grid_hvmv_substation
 	FROM 	grid.otg_ehvhv_bus_data
 	WHERE 	grid.otg_ehvhv_bus_data.base_kv = 110 AND (SELECT TRIM(leading 'n' FROM TRIM(leading 'w' FROM model_draft.ego_grid_hvmv_substation.osm_id))::BIGINT)=grid.otg_ehvhv_bus_data.osm_substation_id; 
 
+DELETE FROM model_draft.ego_grid_hvmv_substation WHERE otg_id IS NULL; 
+
 -- scenario log (project,version,io,schema_name,table_name,script_name,comment)
 SELECT scenario_log('eGo_DP', 'v0.4.0','output','model_draft','ego_grid_hvmv_substation','ego_dp_substation_otg.sql',' ');
 
@@ -40,6 +42,8 @@ UPDATE model_draft.ego_grid_ehv_substation
 	SET otg_id = grid.otg_ehvhv_bus_data.bus_i
 	FROM grid.otg_ehvhv_bus_data
 	WHERE grid.otg_ehvhv_bus_data.base_kv > 110 AND(SELECT TRIM(leading 'n' FROM TRIM(leading 'w' FROM TRIM(leading 'r' FROM model_draft.ego_grid_ehv_substation.osm_id)))::BIGINT)=grid.otg_ehvhv_bus_data.osm_substation_id; 
+
+DELETE FROM model_draft.ego_grid_ehv_substation WHERE otg_id IS NULL;
 
 	-- scenario log (project,version,io,schema_name,table_name,script_name,comment)
 SELECT scenario_log('eGo_DP', 'v0.4.0','output','model_draft','ego_grid_ehv_substation','ego_dp_substation_otg.sql',' ');
