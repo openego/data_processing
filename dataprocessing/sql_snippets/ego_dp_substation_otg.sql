3524652e8aa6fed71682c5ca0768d9e48353b756
IlkaCu <ilka.cussmann@hs-flensburg.de>
2018-05-23 10:21:05 +0200

    Update ego_dp_substation_otg.sql

--- dataprocessing/sql_snippets/ego_dp_substation_otg.sql
+++ dataprocessing/sql_snippets/ego_dp_substation_otg.sql
@@ -23,7 +23,7 @@ ALTER TABLE model_draft.ego_grid_hvmv_substation
 UPDATE model_draft.ego_grid_hvmv_substation
 	SET 	otg_id = grid.otg_ehvhv_bus_data.bus_i
 	FROM 	grid.otg_ehvhv_bus_data
-	WHERE 	grid.otg_ehvhv_bus_data.base_kv = 110 AND (SELECT TRIM(leading 'n' FROM TRIM(leading 'w' FROM model_draft.ego_grid_hvmv_substation.osm_id))::BIGINT)=grid.otg_ehvhv_bus_data.osm_substation_id; 
+	WHERE 	grid.otg_ehvhv_bus_data.base_kv <= 110 AND (SELECT TRIM(leading 'n' FROM TRIM(leading 'w' FROM model_draft.ego_grid_hvmv_substation.osm_id))::BIGINT)=grid.otg_ehvhv_bus_data.osm_substation_id; 
 
 DELETE FROM model_draft.ego_grid_hvmv_substation WHERE otg_id IS NULL; 
 
