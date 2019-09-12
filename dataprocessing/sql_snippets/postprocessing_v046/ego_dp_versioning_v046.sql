40d1086c615d165fe26e2298c4d907ed836a7057
ClaraBuettner <clarabuettner@aol.com>
2019-09-12 15:07:12 +0200

    Add postprocessing scripts for v0.4.6

---
+++ dataprocessing/sql_snippets/postprocessing_v046/ego_dp_versioning_v046.sql
@@ -0,0 +1,326 @@
+﻿/*
+eGo Data Processing result data versioning
+Copy a version from model_draft to OEP schema
+
+__copyright__   = "© Reiner Lemoine Institut"
+__license__     = "GNU Affero General Public License Version 3 (AGPL-3.0)"
+__url__         = "https://github.com/openego/data_processing/blob/master/LICENSE"
+__author__      = "Ludee"
+*/
+
+-- POWERFLOW
+
+-- hv pf bus
+/* DELETE FROM grid.ego_pf_hv_bus
+	WHERE	version = 'v0.4.6'; */
+	
+INSERT INTO grid.ego_pf_hv_bus
+	SELECT	'v0.4.6',
+		*
+	FROM	model_draft.ego_grid_pf_hv_bus;
+
+-- scenario log (project,version,io,schema_name,table_name,script_name,comment)
+SELECT scenario_log('eGo_DP','v0.4.6','result','grid','ego_pf_hv_bus','ego_dp_versioning.sql','versioning only for ego_pf_hv tables');
+
+-- hv pf generator
+/*DELETE SELECT *  FROM grid.ego_pf_hv_bus
+	WHERE	version = 'v0.4.6'; */
+	
+INSERT INTO grid.ego_pf_hv_generator
+	SELECT	'v0.4.6',
+		*
+	FROM	model_draft.ego_grid_pf_hv_generator;
+
+-- scenario log (project,version,io,schema_name,table_name,script_name,comment)
+SELECT scenario_log('eGo_DP','v0.4.6','result','grid','ego_pf_hv_generator','ego_dp_versioning.sql','versioning only for ego_pf_hv tables');
+
+
+-- hv pf generator_pq_set
+/* DELETE FROM grid.ego_pf_hv_generator_pq_set
+	WHERE	version = 'v0.4.6'; */
+	
+INSERT INTO grid.ego_pf_hv_generator_pq_set
+	SELECT	'v0.4.6',
+		*
+	FROM	model_draft.ego_grid_pf_hv_generator_pq_set;
+
+-- scenario log (project,version,io,schema_name,table_name,script_name,comment)
+SELECT scenario_log('eGo_DP','v0.4.6','result','grid','ego_pf_hv_generator_pq_set','ego_dp_versioning.sql','versioning only for ego_pf_hv tables');
+
+
+-- hv pf line
+/* DELETE FROM grid.ego_pf_hv_line
+	WHERE	version = 'v0.4.6'; */
+	
+INSERT INTO grid.ego_pf_hv_line
+	SELECT	'v0.4.6',
+		*
+	FROM	model_draft.ego_grid_pf_hv_line;
+
+-- scenario log (project,version,io,schema_name,table_name,script_name,comment)
+SELECT scenario_log('eGo_DP','v0.4.6','result','grid','ego_pf_hv_line','ego_dp_versioning.sql','versioning only for ego_pf_hv tables');
+
+-- hv pf link
+/* DELETE FROM grid.ego_pf_hv_link
+	WHERE	version = 'v0.4.6'; */
+	
+INSERT INTO grid.ego_pf_hv_link
+	SELECT	'v0.4.6',
+		*
+	FROM	model_draft.ego_grid_pf_hv_link;
+
+-- scenario log (project,version,io,schema_name,table_name,script_name,comment)
+SELECT scenario_log('eGo_DP','v0.4.6','result','grid','ego_pf_hv_line','ego_dp_versioning.sql','versioning only for ego_pf_hv tables');
+
+
+-- hv pf load
+/* DELETE FROM grid.ego_pf_hv_load
+	WHERE	version = 'v0.4.6'; */
+	
+INSERT INTO grid.ego_pf_hv_load
+	SELECT	'v0.4.6',
+		*
+	FROM	model_draft.ego_grid_pf_hv_load;
+
+-- scenario log (project,version,io,schema_name,table_name,script_name,comment)
+SELECT scenario_log('eGo_DP','v0.4.6','result','grid','ego_pf_hv_load','ego_dp_versioning.sql','versioning only for ego_pf_hv tables');
+
+
+-- hv pf load_pq_set
+/* DELETE FROM grid.ego_pf_hv_load_pq_set
+	WHERE	version = 'v0.4.6'; */
+	
+INSERT INTO grid.ego_pf_hv_load_pq_set
+	SELECT	'v0.4.6',
+		*
+	FROM	model_draft.ego_grid_pf_hv_load_pq_set;
+
+-- scenario log (project,version,io,schema_name,table_name,script_name,comment)
+SELECT scenario_log('eGo_DP','v0.4.6','result','grid','ego_pf_hv_load_pq_set','ego_dp_versioning.sql','versioning only for ego_pf_hv tables');
+
+
+-- hv pf source
+/* DELETE FROM grid.ego_pf_hv_source
+	WHERE	version = 'v0.4.6'; */
+	
+INSERT INTO grid.ego_pf_hv_source
+	SELECT	'v0.4.6',
+		*
+	FROM	model_draft.ego_grid_pf_hv_source;
+
+-- scenario log (project,version,io,schema_name,table_name,script_name,comment)
+SELECT scenario_log('eGo_DP','v0.4.6','result','grid','ego_pf_hv_source','ego_dp_versioning.sql','versioning only for ego_pf_hv tables');
+
+
+-- hv pf storage
+/* DELETE FROM grid.ego_pf_hv_storage
+	WHERE	version = 'v0.4.6'; */
+	
+INSERT INTO grid.ego_pf_hv_storage
+	SELECT	'v0.4.6',
+		*
+	FROM	model_draft.ego_grid_pf_hv_storage;
+
+-- scenario log (project,version,io,schema_name,table_name,script_name,comment)
+SELECT scenario_log('eGo_DP','v0.4.6','result','grid','ego_pf_hv_storage','ego_dp_versioning.sql','versioning only for ego_pf_hv tables');
+
+
+-- hv pf storage_pq_set
+/* DELETE FROM grid.ego_pf_hv_storage_pq_set
+	WHERE	version = 'v0.4.6'; */
+	
+INSERT INTO grid.ego_pf_hv_storage_pq_set
+	SELECT	'v0.4.6',
+		*
+	FROM	model_draft.ego_grid_pf_hv_storage_pq_set;
+
+-- scenario log (project,version,io,schema_name,table_name,script_name,comment)
+SELECT scenario_log('eGo_DP','v0.4.6','result','grid','ego_pf_hv_storage_pq_set','ego_dp_versioning.sql','versioning only for ego_pf_hv tables');
+
+
+-- hv pf temp_resolution
+/* DELETE FROM grid.ego_pf_hv_temp_resolution
+	WHERE	version = 'v0.4.6'; */
+	
+INSERT INTO grid.ego_pf_hv_temp_resolution
+	SELECT	'v0.4.6',
+		*
+	FROM	model_draft.ego_grid_pf_hv_temp_resolution;
+
+-- scenario log (project,version,io,schema_name,table_name,script_name,comment)
+SELECT scenario_log('eGo_DP','v0.4.6','result','grid','ego_pf_hv_temp_resolution','ego_dp_versioning.sql','versioning only for ego_pf_hv tables');
+
+
+-- hv pf transformer
+/* DELETE FROM grid.ego_pf_hv_transformer
+	WHERE	version = 'v0.4.6'; */
+	
+INSERT INTO grid.ego_pf_hv_transformer
+	SELECT	'v0.4.6',
+		*
+	FROM	model_draft.ego_grid_pf_hv_transformer;
+
+-- scenario log (project,version,io,schema_name,table_name,script_name,comment)
+SELECT scenario_log('eGo_DP','v0.4.6','result','grid','ego_pf_hv_transformer','ego_dp_versioning.sql','versioning only for ego_pf_hv tables');
+
+-- hv pf extension bus
+/* DELETE FROM grid.ego_pf_hv_extension_bus
+	WHERE	version = 'v0.4.6'; */
+	
+INSERT INTO grid.ego_pf_hv_extension_bus
+	SELECT	'v0.4.6',
+		 scn_name,  bus_id,  v_nom,  current_type,  v_mag_pu_min,  v_mag_pu_max,  geom	
+	FROM	model_draft.ego_grid_pf_hv_extension_bus
+	WHERE scn_name IN ('extension_nep2035_b2', 'decommissioning_nep2035_b2','extension_nep2035_confirmed', 'decommissioning_nep2035_confirmed', 'extension_BE_NO_NEP 2035', 'extension_BE_NO_eGo 100' );
+-- scenario log (project,version,io,schema_name,table_name,script_name,comment)
+SELECT scenario_log('eGo_DP','v0.4.6','result','grid','ego_pf_hv_extension_bus','ego_dp_versioning.sql','versioning only for ego_pf_hv tables');
+
+-- hv pf extension generator
+ /*DELETE FROM grid.ego_pf_hv_extension_generator
+	WHERE	version = 'v0.4.6'; */
+	
+INSERT INTO grid.ego_pf_hv_extension_generator
+	SELECT	'v0.4.6',
+		*
+	FROM	model_draft.ego_grid_pf_hv_extension_generator
+	WHERE scn_name IN ('extension_nep2035_b2', 'decommissioning_nep2035_b2','extension_nep2035_confirmed', 'decommissioning_nep2035_confirmed', 'extension_BE_NO_NEP 2035', 'extension_BE_NO_eGo 100' );
+
+-- scenario log (project,version,io,schema_name,table_name,script_name,comment)
+SELECT scenario_log('eGo_DP','v0.4.6','result','grid','ego_pf_hv_extension_generator','ego_dp_versioning.sql','versioning only for ego_pf_hv tables');
+
+
+-- hv pf extension generator_pq_set
+/* DELETE FROM grid.ego_pf_hv_generator_pq_set
+	WHERE	version = 'v0.4.6'; */
+	
+INSERT INTO grid.ego_pf_hv_extension_generator_pq_set
+	SELECT	'v0.4.6',
+		*
+	FROM	model_draft.ego_grid_pf_hv_extension_generator_pq_set
+	WHERE scn_name IN ('extension_nep2035_b2', 'decommissioning_nep2035_b2','extension_nep2035_confirmed', 'decommissioning_nep2035_confirmed', 'extension_BE_NO_NEP 2035', 'extension_BE_NO_eGo 100' );
+
+-- scenario log (project,version,io,schema_name,table_name,script_name,comment)
+SELECT scenario_log('eGo_DP','v0.4.6','result','grid','ego_pf_hv_extension_generator_pq_set','ego_dp_versioning.sql','versioning only for ego_pf_hv tables');
+
+
+-- hv pf extension line
+/* DELETE FROM grid.ego_pf_hv_extension_line
+	WHERE	version = 'v0.4.6'; */
+	
+INSERT INTO grid.ego_pf_hv_extension_line
+	SELECT	'v0.4.6',
+		 scn_name,  line_id ,  bus0, bus1, x, r, g, b, s_nom, s_nom_extendable, s_nom_min, s_nom_max, capital_cost, length, cables, frequency, terrain_factor, geom, topo, v_nom, project, project_id
+	FROM	model_draft.ego_grid_pf_hv_extension_line
+	WHERE scn_name IN ('extension_nep2035_b2', 'decommissioning_nep2035_b2','extension_nep2035_confirmed', 'decommissioning_nep2035_confirmed', 'extension_BE_NO_NEP 2035', 'extension_BE_NO_eGo 100' );
+
+-- scenario log (project,version,io,schema_name,table_name,script_name,comment)
+SELECT scenario_log('eGo_DP','v0.4.6','result','grid','ego_pf_hv_extension_line','ego_dp_versioning.sql','versioning only for ego_pf_hv tables');
+
+-- hv pf extension link
+/* DELETE FROM grid.ego_pf_hv_extension_link
+	WHERE	version = 'v0.4.6'; */
+	
+INSERT INTO grid.ego_pf_hv_extension_link
+	SELECT	'v0.4.6',
+		 scn_name, link_id, bus0, bus1, efficiency, marginal_cost, p_nom, p_nom_extendable, p_nom_min, p_nom_max, capital_cost, length, terrain_factor,geom, topo, project, project_id
+	FROM	model_draft.ego_grid_pf_hv_extension_link
+	WHERE scn_name IN ('extension_nep2035_b2', 'decommissioning_nep2035_b2','extension_nep2035_confirmed', 'decommissioning_nep2035_confirmed', 'extension_BE_NO_NEP 2035', 'extension_BE_NO_eGo 100' );
+
+-- scenario log (project,version,io,schema_name,table_name,script_name,comment)
+SELECT scenario_log('eGo_DP','v0.4.6','result','grid','ego_pf_hv_extension_link','ego_dp_versioning.sql','versioning only for ego_pf_hv tables');
+
+
+-- hv pf extension load
+/* DELETE FROM grid.ego_pf_hv_extension_load
+	WHERE	version = 'v0.4.6'; */
+	
+INSERT INTO grid.ego_pf_hv_extension_load
+	SELECT	'v0.4.6',
+		*
+	FROM	model_draft.ego_grid_pf_hv_extension_load
+	WHERE scn_name IN ('extension_nep2035_b2', 'decommissioning_nep2035_b2','extension_nep2035_confirmed', 'decommissioning_nep2035_confirmed', 'extension_BE_NO_NEP 2035', 'extension_BE_NO_eGo 100' );
+
+-- scenario log (project,version,io,schema_name,table_name,script_name,comment)
+SELECT scenario_log('eGo_DP','v0.4.6','result','grid','ego_pf_hv_extension_load','ego_dp_versioning.sql','versioning only for ego_pf_hv tables');
+
+
+-- hv pf extension load_pq_set
+/* DELETE FROM grid.ego_pf_hv_extension_load_pq_set
+	WHERE	version = 'v0.4.6'; */
+	
+INSERT INTO grid.ego_pf_hv_extension_load_pq_set
+	SELECT	'v0.4.6',
+		*
+	FROM	model_draft.ego_grid_pf_hv_extension_load_pq_set
+	WHERE scn_name IN ('extension_nep2035_b2', 'decommissioning_nep2035_b2','extension_nep2035_confirmed', 'decommissioning_nep2035_confirmed', 'extension_BE_NO_NEP 2035', 'extension_BE_NO_eGo 100' );
+
+-- scenario log (project,version,io,schema_name,table_name,script_name,comment)
+SELECT scenario_log('eGo_DP','v0.4.6','result','grid','ego_pf_hv_extension_load_pq_set','ego_dp_versioning.sql','versioning only for ego_pf_hv tables');
+
+
+-- hv pf extension source
+/* DELETE FROM grid.ego_pf_hv_source
+	WHERE	version = 'v0.4.6'; */
+	
+INSERT INTO grid.ego_pf_hv_extension_source
+	SELECT	'v0.4.6',
+		source_id, name, co2_emissions, commentary
+	FROM	model_draft.ego_grid_pf_hv_extension_source;
+
+-- scenario log (project,version,io,schema_name,table_name,script_name,comment)
+SELECT scenario_log('eGo_DP','v0.4.6','result','grid','ego_pf_hv_extension_source','ego_dp_versioning.sql','versioning only for ego_pf_hv tables');
+
+
+-- hv pf extension storage
+/* DELETE FROM grid.ego_pf_hv_extension_storage
+	WHERE	version = 'v0.4.6'; */
+	
+INSERT INTO grid.ego_pf_hv_extension_storage
+	SELECT	'v0.4.6',
+		*
+	FROM	model_draft.ego_grid_pf_hv_extension_storage
+	WHERE scn_name IN ('extension_nep2035_b2', 'decommissioning_nep2035_b2','extension_nep2035_confirmed', 'decommissioning_nep2035_confirmed', 'extension_BE_NO_NEP 2035', 'extension_BE_NO_eGo 100' );
+
+-- scenario log (project,version,io,schema_name,table_name,script_name,comment)
+SELECT scenario_log('eGo_DP','v0.4.6','result','grid','ego_pf_hv_extension_storage','ego_dp_versioning.sql','versioning only for ego_pf_hv tables');
+
+
+-- hv pf extension storage_pq_set
+/* DELETE FROM grid.ego_pf_hv_extension_storage_pq_set
+	WHERE	version = 'v0.4.6'; */
+	
+INSERT INTO grid.ego_pf_hv_extension_storage_pq_set
+	SELECT	'v0.4.6',
+		*
+	FROM	model_draft.ego_grid_pf_hv_extension_storage_pq_set
+	WHERE scn_name IN ('extension_nep2035_b2', 'decommissioning_nep2035_b2','extension_nep2035_confirmed', 'decommissioning_nep2035_confirmed', 'extension_BE_NO_NEP 2035', 'extension_BE_NO_eGo 100' );
+
+-- scenario log (project,version,io,schema_name,table_name,script_name,comment)
+SELECT scenario_log('eGo_DP','v0.4.6','result','grid','ego_pf_hv_extension_storage_pq_set','ego_dp_versioning.sql','versioning only for ego_pf_hv tables');
+
+
+-- hv pf extension temp_resolution
+/* DELETE FROM grid.ego_pf_hv_extension_temp_resolution
+	WHERE	version = 'v0.4.6'; */
+	
+INSERT INTO grid.ego_pf_hv_extension_temp_resolution
+	SELECT	'v0.4.6',
+		*
+	FROM	model_draft.ego_grid_pf_hv_extension_temp_resolution;
+
+-- scenario log (project,version,io,schema_name,table_name,script_name,comment)
+SELECT scenario_log('eGo_DP','v0.4.6','result','grid','ego_pf_hv_extension_temp_resolution','ego_dp_versioning.sql','versioning only for ego_pf_hv tables');
+
+
+-- hv pf extension transformer
+/* DELETE FROM grid.ego_pf_hv_extension_transformer
+	WHERE	version = 'v0.4.6'; */
+	
+INSERT INTO grid.ego_pf_hv_extension_transformer
+	SELECT	'v0.4.6',
+		  scn_name, trafo_id, bus0,bus1, x, r, g, b, s_nom, s_nom_extendable, s_nom_min, s_nom_max, tap_ratio, phase_shift, capital_cost, geom, topo, project
+	FROM	model_draft.ego_grid_pf_hv_extension_transformer
+	WHERE scn_name IN ('extension_nep2035_b2', 'decommissioning_nep2035_b2','extension_nep2035_confirmed', 'decommissioning_nep2035_confirmed', 'extension_BE_NO_NEP 2035', 'extension_BE_NO_eGo 100' );
+
+-- scenario log (project,version,io,schema_name,table_name,script_name,comment)
+SELECT scenario_log('eGo_DP','v0.4.6','result','grid','ego_pf_hv_extension_transformer','ego_dp_versioning.sql','versioning only for ego_pf_hv tables');
---
+++ dataprocessing/sql_snippets/postprocessing_v046/ego_pp_add_extensions_to_pf_tables.sql
@@ -0,0 +1,112 @@
+﻿--- wind offshore---
+UPDATE grid.ego_pf_hv_generator a
+SET p_nom = w_off.p_nom_new
+FROM 
+(SELECT pf.generator_id, ext.p_nom + pf.p_nom as p_nom_new
+FROM model_draft.ego_grid_pf_hv_extension_generator ext, model_draft.ego_grid_pf_hv_generator pf
+WHERE ext.bus = pf.bus 
+AND pf.source = ext.source 
+AND pf.scn_name = 'NEP 2035'
+AND ext.scn_name = 'extension_bugfix_wind_offshore'
+AND pf.generator_id IN (SELECT d.generator_id
+			FROM model_draft.ego_grid_pf_hv_generator d
+			WHERE d.source = 17 AND 
+			d.bus = pf.bus AND d.scn_name = pf.scn_name
+			AND p_nom = (SELECT MAX(p_nom) FROM model_draft.ego_grid_pf_hv_generator
+				WHERE source = 17 AND 
+				bus =  pf.bus AND scn_name = pf.scn_name))
+				) as w_off
+
+WHERE a.version = 'v0.4.6' AND a.generator_id IN (w_off.generator_id) AND a.scn_name = 'NEP 2035';
+
+/*SELECT sum(p_nom) FROM grid.ego_pf_hv_generator a
+WHERE a.version = 'v0.4.6' AND a.source = 17 AND a.scn_name = 'NEP 2035';*/
+
+UPDATE grid.ego_pf_hv_generator a
+SET p_nom = w_off.p_nom_new
+FROM
+(SELECT pf.generator_id, ext.p_nom + pf.p_nom as p_nom_new
+FROM model_draft.ego_grid_pf_hv_extension_generator ext, model_draft.ego_grid_pf_hv_generator pf
+WHERE ext.bus = pf.bus 
+AND pf.source = ext.source 
+AND pf.scn_name = 'eGo 100'
+AND ext.scn_name = 'extension_bugfix_wind_offshore_eGo100'
+AND pf.generator_id IN (SELECT d.generator_id
+			FROM model_draft.ego_grid_pf_hv_generator d
+			WHERE d.source = 17 AND 
+			d.bus = pf.bus AND d.scn_name = pf.scn_name
+			AND p_nom = (SELECT MAX(p_nom) FROM model_draft.ego_grid_pf_hv_generator
+				WHERE source = 17 AND 
+				bus =  pf.bus AND scn_name = pf.scn_name))
+				) as w_off
+
+WHERE a.version = 'v0.4.6' AND a.generator_id IN (w_off.generator_id) AND a.scn_name = 'eGo 100';
+
+/*SELECT sum(p_nom) FROM grid.ego_pf_hv_generator a
+WHERE a.version = 'v0.4.6' AND a.source = 17 AND a.scn_name = 'eGo 100';*/
+
+--- wind onshore ---
+UPDATE grid.ego_pf_hv_generator a
+SET p_nom = w_on.p_nom_new
+FROM
+(SELECT pf.bus, pf.generator_id,  pf.p_nom - decom.p_nom as p_nom_new
+FROM model_draft.ego_grid_pf_hv_extension_generator decom, model_draft.ego_grid_pf_hv_generator pf
+WHERE decom.bus = pf.bus 
+AND pf.source = decom.source 
+AND pf.scn_name = 'NEP 2035'
+AND decom.scn_name = 'decommissioning_bugfix_pv_wind_nep'
+AND pf.source = 13
+AND pf.generator_id  = decom.generator_id) as w_on
+WHERE a.version = 'v0.4.6' AND a.generator_id IN (w_on.generator_id) AND a.scn_name = 'NEP 2035';
+
+/*SELECT sum(p_nom) FROM grid.ego_pf_hv_generator a
+WHERE a.version = 'v0.4.6' AND a.source = 13 AND a.scn_name = 'NEP 2035';*/
+--- solar ---
+
+UPDATE grid.ego_pf_hv_generator a
+SET p_nom = w_on.p_nom_new
+FROM
+(SELECT pf.bus, pf.generator_id,  pf.p_nom - decom.p_nom as p_nom_new
+FROM model_draft.ego_grid_pf_hv_extension_generator decom, model_draft.ego_grid_pf_hv_generator pf
+WHERE decom.bus = pf.bus 
+AND pf.source = decom.source 
+AND pf.scn_name = 'NEP 2035'
+AND decom.scn_name = 'decommissioning_bugfix_pv_wind_nep'
+AND pf.source = 12
+AND pf.generator_id  = decom.generator_id) as w_on
+WHERE a.version = 'v0.4.6' AND a.generator_id IN (w_on.generator_id) AND a.scn_name = 'NEP 2035';
+/*SELECT sum(p_nom) FROM grid.ego_pf_hv_generator a
+WHERE a.version = 'v0.4.6' AND a.source = 12 AND a.scn_name = 'NEP 2035';*/
+
+DELETE FROM grid.ego_pf_hv_generator_pq_set WHERE generator_id IN 
+(SELECT generator_id FROM grid.ego_pf_hv_generator WHERE p_nom <= 0 AND version = 'v0.4.6');
+DELETE FROM grid.ego_pf_hv_generator WHERE p_nom <= 0 AND version = 'v0.4.6';
+
+UPDATE grid.ego_pf_hv_generator a
+SET p_nom = chp.p_nom_new
+FROM
+(SELECT pf.generator_id, ext.p_nom + pf.p_nom as p_nom_new
+FROM model_draft.ego_grid_pf_hv_extension_generator ext, model_draft.ego_grid_pf_hv_generator pf
+WHERE ext.bus = pf.bus 
+AND pf.source = ext.source 
+AND pf.scn_name = 'NEP 2035'
+AND ext.scn_name = 'extension_chp_nep'
+AND pf.generator_id IN (SELECT d.generator_id
+			FROM model_draft.ego_grid_pf_hv_generator d
+			WHERE d.source = 1 AND 
+			d.bus = pf.bus AND d.scn_name = pf.scn_name
+			AND p_nom = (SELECT MAX(p_nom) FROM model_draft.ego_grid_pf_hv_generator
+				WHERE source = 1 AND 
+				bus =  pf.bus AND scn_name = pf.scn_name))
+				) as chp
+WHERE a.version = 'v0.4.6' AND a.generator_id IN (chp.generator_id) AND a.scn_name = 'NEP 2035';
+
+
+INSERT INTO grid.ego_pf_hv_generator
+SELECT 'v0.4.6', 'NEP 2035', generator_id,bus,dispatch, control ,  p_nom,  p_nom_extendable, p_nom_min, p_nom_max, p_min_pu_fixed, p_max_pu_fixed,sign, source, marginal_cost, capital_cost, efficiency
+FROM model_draft.ego_grid_pf_hv_extension_generator 
+WHERE scn_name = 'extension_chp_nep'
+AND bus NOT IN (SELECT bus FROM model_draft.ego_grid_pf_hv_generator WHERE scn_name = 'NEP 2035' AND source = 1)
+/*SELECT sum(p_nom) FROM grid.ego_pf_hv_generator a
+WHERE a.version = 'v0.4.6' AND a.source = 1 AND a.scn_name = 'NEP 2035';*/
+
