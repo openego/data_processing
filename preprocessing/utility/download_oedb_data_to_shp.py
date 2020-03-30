be3c38100d8ac179af1026020b08718f2ce3390f
Guido Pleßmann <guido.plessmann@rl-institut.de>
2020-03-30 10:56:13 +0200

    Rework shp download scripts

--- preprocessing/utility/download_oedb_data_to_shp.py
+++ preprocessing/utility/download_oedb_data_to_shp.py
@@ -1,7 +1,6 @@
 import pandas as pd
 from egoio.tools import db
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy import func
 import geopandas as gdp
 from shapely import wkt
 import zipfile
@@ -18,7 +17,7 @@ def zipdir(path, ziph):
             ziph.write(os.path.join(root, file), arcname=file)
 
 
-def oedbtable2shp(table_orm, filename, crs, geometry_col, index_col):
+def oedbtable2shp(table_query, filename, crs, geometry_col, index_col):
 
     shp_dir = os.path.join(DOWNLOADDIR, filename.replace(".shp", ""))
     os.makedirs(shp_dir, exist_ok=True)
@@ -28,10 +27,6 @@ def oedbtable2shp(table_orm, filename, crs, geometry_col, index_col):
     engine_oedb = db.connection(readonly=True)
     session = sessionmaker(bind=engine_oedb)()
 
-    table_query = session.query(
-        table_orm.region_key,
-        func.ST_AsText(func.ST_Transform(table_orm.geom, crs)).label(geometry_col))
-
     table_df = pd.read_sql_query(table_query.statement,
                                  session.bind,
                                  index_col=index_col)
@@ -39,7 +34,7 @@ def oedbtable2shp(table_orm, filename, crs, geometry_col, index_col):
     table_df[geometry_col] = table_df[geometry_col].apply(wkt.loads)
     table_gdf = gdp.GeoDataFrame(table_df,
                                  geometry=geometry_col,
-                                 crs={'init': 'epsg:{}'.format(crs)})
+                                 crs=crs)
 
     table_gdf.to_file(shp_filename)
 
--- preprocessing/utility/vernetzen_wind_potential_areas_to_shp.py
+++ preprocessing/utility/vernetzen_wind_potential_areas_to_shp.py
@@ -1,6 +1,10 @@
 from egoio.db_tables.supply import VernetzenWindPotentialArea
 import os
 from preprocessing.utility.download_oedb_data_to_shp import oedbtable2shp
+from sqlalchemy import func
+from sqlalchemy.orm import sessionmaker
+from egoio.tools import db
+
 
 
 CRS = 4326
@@ -9,7 +13,15 @@ CRS = 4326
 def vernetzen_windpotential_areas2shp():
 
     filename = "vernetzen_wind_potential_areas.shp"
-    oedbtable2shp(VernetzenWindPotentialArea, filename, CRS, "geom", "region_key")
+
+    engine_oedb = db.connection(readonly=True)
+    session = sessionmaker(bind=engine_oedb)()
+
+    table_query = session.query(
+        VernetzenWindPotentialArea.region_key,
+        func.ST_AsText(func.ST_Transform(VernetzenWindPotentialArea.geom, CRS)).label("geom"))
+
+    oedbtable2shp(table_query, filename, CRS, "geom", "region_key")
 
 
 if __name__== "__main__":
