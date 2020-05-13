9a60cb8e2d844e65fbaadadb9bb5494cfc7621f2
Guido Pleßmann <guido.plessmann@rl-institut.de>
2020-05-13 16:16:22 +0200

    Add osmTGmod to DP-pre pipeline

--- preprocessing/utility/osmTGmod_to_shp.py
+++ preprocessing/utility/osmTGmod_to_shp.py
@@ -1,10 +1,11 @@
 from egoio.db_tables.grid import OtgEhvhvBusDatum, OtgEhvhvBranchDatum, OtgEhvhvDclineDatum, OtgEhvhvResultsMetadatum
 import os
 from preprocessing.utility.download_oedb_data_to_shp import oedbtable2shp
+from preprocessing.utility.utils import DOWNLOADDIR, zipdir
 from sqlalchemy import func
 from sqlalchemy.orm import sessionmaker
 from egoio.tools import db
-
+import zipfile
 
 
 CRS = 4326
@@ -12,10 +13,11 @@ CRS = 4326
 
 def osmTGmod2shp():
 
-    filename_bus = "osmTGmod_bus.shp"
-    filename_branch = "osmTGmod_branch.shp"
-    filename_dcline = "osmTGmod_dcline.shp"
-    filename_results = "osmTGmod_results.csv"
+    filename_zip = "otg_ehvhv_data.zip"
+    filename_bus = "otg_ehvhv_bus_data.shp"
+    filename_branch = "otg_ehvhv_branch_data.shp"
+    filename_dcline = "otg_ehvhv_dcline_data.shp"
+    filename_results = "otg_ehvhv_results_metadata.csv"
 
     engine_oedb = db.connection(readonly=True)
     session = sessionmaker(bind=engine_oedb)()
@@ -106,11 +108,23 @@ def osmTGmod2shp():
         OtgEhvhvResultsMetadatum.user_comment,
     )
 
-    oedbtable2shp(bus_query, filename_bus, CRS, "geom", "view_id")
-    oedbtable2shp(branch_query, filename_branch, CRS, "geom", "view_id")
-    oedbtable2shp(dcline_query, filename_dcline, CRS, "geom", "view_id")
-    oedbtable2shp(results_query, filename_results, CRS, None, "id")
+    oedbtable2shp(bus_query, filename_bus, CRS, "geom", "view_id", zip_it=False)
+    oedbtable2shp(branch_query, filename_branch, CRS, "geom", "view_id", zip_it=False)
+    oedbtable2shp(dcline_query, filename_dcline, CRS, "geom", "view_id", zip_it=False)
+    oedbtable2shp(results_query, filename_results, CRS, None, "id", zip_it=False)
 
+    # zip all files into a single archive
+    directories = [os.path.join(DOWNLOADDIR, os.path.splitext(f)[0]) for f in
+                   [filename_bus, filename_branch, filename_dcline, filename_results]
+                   ]
+    with zipfile.ZipFile(
+        os.path.join(DOWNLOADDIR, filename_zip),
+        mode='w',
+        compression=zipfile.ZIP_DEFLATED) as zf:
+
+        for directory in directories:
+            zipdir(directory, zf)
+    zf.close()
 
 if __name__== "__main__":
     osmTGmod2shp()
